8cf48b766935b6c3904e26e8e6ba4249563d020c
Victor Flores <victor.floress@usach.cl>
2013-04-12 11:52:15 -0400

    probando git2

--- CodeIgniter_2.1.3/application/views/templates/menu_superior.php
+++ CodeIgniter_2.1.3/application/views/templates/menu_superior.php
@@ -6,9 +6,9 @@
 			<a href="#"><img src="/manteka/img/icons/mail-message.png" alt="Correo" height="50" width="50">Correo</a>
 		</li>
 		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Profesor</a></li>
-		<li><a href="#"><img src="/manteka/img/icons/seccion.png" alt="Correo" height="50" width="50">Seccion</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/seccion.png"  alt="Correo" height="50" width="50">Seccion</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/university.png" alt="Correo" height="50" width="50">Modulo</a></li>
-		<li><a href="#"><img src="/manteka/img/icons/sala.png" alt="Correo" height="50" width="50">Sala</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/sala.png"  alt="Correo" height="50" width="50">Sala</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Alumno</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/inform.png" alt="Correo" height="50" width="50">Informe</a></li>
 		
