c4e71dd58b6ae91eb2965d2f31caa4cdc3ea52ae
Victor Flores <victor.floress@usach.cl>
2013-04-12 11:58:47 -0400

    probando 3

--- CodeIgniter_2.1.3/application/views/templates/menu_superior.php
+++ CodeIgniter_2.1.3/application/views/templates/menu_superior.php
@@ -6,7 +6,7 @@
 			<a href="#"><img src="/manteka/img/icons/mail-message.png" alt="Correo" height="50" width="50">Correo</a>
 		</li>
 		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Profesor</a></li>
-		<li><a href="#"><img src="/manteka/img/icons/seccion.png"  alt="Correo" height="50" width="50">Seccion</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/seccion.png" alt="Correo" height="50" width="50">Sección</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/university.png" alt="Correo" height="50" width="50">Modulo</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/sala.png"  alt="Correo" height="50" width="50">Sala</a></li>
 		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Alumno</a></li>
