79c1c0f6eec2bb6c9c898a5f13130c7cc0cb30c5
Victor Flores <victor.floress@usach.cl>
2013-04-12 11:49:08 -0400

    "Agregados iconos al menu principal"

--- CodeIgniter_2.1.3/application/views/templates/menu_superior.php
+++ CodeIgniter_2.1.3/application/views/templates/menu_superior.php
@@ -3,14 +3,14 @@
 <div>
 	<ul class="nav nav-tabs">
 		<li class="active">
-			<a href="#">Correo</a>
+			<a href="#"><img src="/manteka/img/icons/mail-message.png" alt="Correo" height="50" width="50">Correo</a>
 		</li>
-		<li><a href="#">Profesor</a></li>
-		<li><a href="#">Seccion</a></li>
-		<li><a href="#">Modulo</a></li>
-		<li><a href="#">Sala</a></li>
-		<li><a href="#">Alumno</a></li>
-		<li><a href="#">Informe</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Profesor</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/seccion.png" alt="Correo" height="50" width="50">Seccion</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/university.png" alt="Correo" height="50" width="50">Modulo</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/sala.png" alt="Correo" height="50" width="50">Sala</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/system-users.png" alt="Correo" height="50" width="50">Alumno</a></li>
+		<li><a href="#"><img src="/manteka/img/icons/inform.png" alt="Correo" height="50" width="50">Informe</a></li>
 		
 	</ul>
 </div>
