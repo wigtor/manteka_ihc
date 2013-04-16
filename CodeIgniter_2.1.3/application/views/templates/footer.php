7700268ea7f1f4e7d6c17858682fcaa650247e3e
Victor Flores <victor.floress@usach.cl>
2013-04-16 19:38:10 -0400

    Arreglado tamaño de footer

--- CodeIgniter_2.1.3/application/views/templates/footer.php
+++ CodeIgniter_2.1.3/application/views/templates/footer.php
@@ -2,9 +2,9 @@
 <footer>
 	Equipo de desarrollo de IHC 1-2013.
 	Compatible con:
-	<a class="btn_with_icon_solo" href="http://windows.microsoft.com/es-es/internet-explorer/download-ie">e</a>
-	<a class="btn_with_icon_solo" href="http://www.google.com/chrome?hl=es">g</a>
-	<a class="btn_with_icon_solo" href="http://www.mozilla.org/es-CL/firefox/fx/">d</a>
-	<a class="btn_with_icon_solo" href="http://www.opera.com/download/">f</a>
-	<a class="btn_with_icon_solo" href="http://www.apple.com/es/safari">h</a>
+	<a class="btn_with_icon_solo_footer" href="http://windows.microsoft.com/es-es/internet-explorer/download-ie">e</a>
+	<a class="btn_with_icon_solo_footer" href="http://www.google.com/chrome?hl=es">g</a>
+	<a class="btn_with_icon_solo_footer" href="http://www.mozilla.org/es-CL/firefox/fx/">d</a>
+	<a class="btn_with_icon_solo_footer" href="http://www.opera.com/download/">f</a>
+	<a class="btn_with_icon_solo_footer" href="http://www.apple.com/es/safari">h</a>
 </footer>
