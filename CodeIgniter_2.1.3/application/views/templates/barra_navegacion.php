819d904996106280fa8192c9ed07f40b16de684a
Victor Flores <victor.floress@usach.cl>
2013-04-16 19:35:12 -0400

    Arreglados íconos de footer

--- CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
+++ CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
@@ -1,9 +1,9 @@
 <!-- Barra con los botones undo y redo -->
 <div class="barra_navegacion pull-top">
 	<div class="span6">
-		<a class="btn_with_icon">&#60;</a>
+		<div class="btn btn_with_icon_solo">&#60;</div>
 	</div>
 	<div class="span6">
-		<a class="btn_with_icon">&#61;</a>
+		<div class="btn btn_with_icon_solo">&#61;</div>
 	</div>
 </div>
