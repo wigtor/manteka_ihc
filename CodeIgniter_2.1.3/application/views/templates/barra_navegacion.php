2fe4bdda1331cc419f7990756bd03ba83665acc7
Victor Flores <victor.floress@usach.cl>
2013-04-16 12:29:29 -0400

    Cambiados los íconos para usar fonts-face

--- CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
+++ CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
@@ -1,9 +1,9 @@
 <!-- Barra con los botones undo y redo -->
 <div class="barra_navegacion pull-top">
 	<div class="span6">
-		<img class="btn btn_sin_padding" src="/manteka/img/icons/undo2.png" alt="undo" height="28px" width="28px">
+		<a class="btn_with_icon">&#60;</a>
 	</div>
 	<div class="span6">
-		<img class="btn btn_sin_padding" src="/manteka/img/icons/redo2.png" alt="redo" height="28px" width="28px">
+		<a class="btn_with_icon">&#61;</a>
 	</div>
 </div>
--- CodeIgniter_2.1.3/application/views/templates/menu_superior.php
+++ CodeIgniter_2.1.3/application/views/templates/menu_superior.php
@@ -13,7 +13,7 @@
 				echo '<li>';
 			}
 		?>
-			<a class="btn_with_icon" href="<?php echo site_url("Correo/index") ?>">MCorreos</a>
+			<a class="btn_with_icon" href="<?php echo site_url("Correo/index") ?>">M Correos</a>
 		</li>
 		<?php 
 			if ($menuSuperiorAbierto == "Docentes") {
@@ -23,7 +23,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Profesores/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/user4.png" alt="Correo">Docentes</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Profesores/index") ?>">L Docentes</a></li>
 		<?php 
 			if ($menuSuperiorAbierto == "Secciones") {
 				echo '<li class="active">';
@@ -32,7 +32,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Secciones/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/users.png" alt="Correo">Secciones</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Secciones/index") ?>">K Secciones</a></li>
 		<?php 
 			if ($menuSuperiorAbierto == "Planificacion") {
 				echo '<li class="active">';
@@ -41,7 +41,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Planificacion/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/calendar2.png" alt="Correo">Planificación</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Planificacion/index") ?>">É Planificación</a></li>
 		<?php 
 			if ($menuSuperiorAbierto == "Salas") {
 				echo '<li class="active">';
@@ -50,7 +50,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Salas/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/library.png"  alt="Correo">Salas</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Salas/index") ?>">S Salas</a></li>
 		<?php 
 			if ($menuSuperiorAbierto == "Alumnos") {
 				echo '<li class="active">';
@@ -59,7 +59,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Alumnos/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/user.png" alt="Correo" height="50px" width="50px">Alumnos</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Alumnos/index") ?>">Ù Alumnos</a></li>
 		<?php 
 			if ($menuSuperiorAbierto == "Informes") {
 				echo '<li class="active">';
@@ -68,7 +68,7 @@
 				echo '<li>';
 			}
 		?>
-		<a href="<?php echo site_url("Informes/index") ?>"><img class="icon_menu_superior" src="/<?php echo config_item('dir_alias') ?>/img/icons/book.png" alt="Correo" height="50px" width="50px">Informes</a></li>
+		<a class="btn_with_icon" href="<?php echo site_url("Informes/index") ?>">E Informes</a></li>
 		
 	</ul>
 	</div>
