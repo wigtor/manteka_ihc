2939bfadba556e26caad385c157526a020c192d7
marco baeza <marco.baeza@usach.cl>
2013-04-16 02:02:47 -0400

    Arreglando barra de usuario

--- CodeIgniter_2.1.3/application/views/templates/barra_usuario.php
+++ CodeIgniter_2.1.3/application/views/templates/barra_usuario.php
@@ -1,36 +1,33 @@
 <!-- Barra superior que contiene el nombre del usuario, el menu, cerrar sesión y botón de ayuda -->
-<div class="navbar barra_usuario">
+<!--div class="navbar barra_usuario">
 	<!-- <div class="navbar-inner hacerTransparente"> -->
-		<ul class="nav pull-right botones_barra_usuario">
+		<ul class="nav nav-pills pull-right" style="background-color: #ffffff; ">
 			<li>
-				<button class="btn btn_sin_padding btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
-					<?php echo 'Paz Bustos' ?>
+				<!--button class="btn btn_sin_padding btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
+					<?php  ?>
 					<span class="caret"></span>
-				</button>
-				<ul class="dropdown-menu btn_sin_padding">
-					<li>
-						Editar perfil
-					</li>
-					<li>
-						Cambiar correo electrónico
-					</li>
-					<li>
-						Cambiar contraseña
-					</li>
-				</ul>
+				</button-->
+				<li class="dropdown active btn_with_icon" style ="padding-left: 2px; padding-bottom: 0px;">
+				    <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="font-size: 12px; padding-bottom: 5px; padding-top: 6px; padding-left: 8px; padding-right: 5px;">
+				        L <?php echo "Paz Bustos"?>
+				        <b class="caret"></b>
+				      </a>
+				    <ul class="dropdown-menu">
+				    	<li>
+							<a href="#">Modificar Usuario</a>
+				    	</li>
+				    	<li>
+							<a href="#">Cambiar contraseña</a>
+				    	</li>
+				    	<li>
+							<a href="#">Cerrar Sesión</a>
+				    	</li>
+				    </ul>
+				  </li>
 			</li>
-			<li>
-				<button class="btn btn_sin_padding" href="#">
-					Cerrar sesión
-					
-				</button>
-				
-			</li>
-			<li>
-				<button class="btn btn_sin_padding" href="#">
-					Ayuda
-				</button>
+			<li class="active">
+				<a style="font-size: 12px; padding-bottom: 5px; padding-top: 6px; padding-left: 10px; padding-right: 10px;" href="#">
+					 <?php echo "Ayuda"?> </a>
 			</li>
 		</ul>
 	<!--</div> -->
-</div >
