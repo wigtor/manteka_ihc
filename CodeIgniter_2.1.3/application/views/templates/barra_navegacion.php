0a0790bb6311c04333a1c33a23d27cc7d916a0e3
Victor Flores <victor.floress@usach.cl>
2013-04-16 23:47:00 -0400

    borrados iconos innecesarios

--- CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
+++ CodeIgniter_2.1.3/application/views/templates/barra_navegacion.php
@@ -1,9 +1,13 @@
 <!-- Barra con los botones undo y redo -->
 <div class="barra_navegacion pull-top">
 	<div class="span6">
-		<div class="btn btn_with_icon_solo">&#60;</div>
+		<div class="btn btn_with_icon_solo">
+			<img src="/<?php echo config_item('dir_alias') ?>/img/icons/undo2.png" alt="9" style="width: 15px; height: 15px;">
+		</div>
 	</div>
 	<div class="span6">
-		<div class="btn btn_with_icon_solo">&#61;</div>
+		<div class="btn btn_with_icon_solo">
+			<img src="/<?php echo config_item('dir_alias') ?>/img/icons/redo2.png" alt=":" style="width: 15px; height: 15px;">
+		</div>
 	</div>
 </div>
--- CodeIgniter_2.1.3/application/views/templates/barra_progreso_atras_siguiente.php
+++ CodeIgniter_2.1.3/application/views/templates/barra_progreso_atras_siguiente.php
@@ -1,8 +1,8 @@
 <!-- Barra con los botones undo y redo -->
 <div class="span7">
 	<div class="progress">
-		<div class="bar" style="width: 20%;"><p>Seleccionar destinatarios</p></div>
-		<div class="bar" style="width: 20%;"><p>Seleccionar plantillas</p></div>
+		<div class="bar" style="width: 20%;"><p>Destinatarios</p></div>
+		<div class="bar" style="width: 20%;"><p>Plantillas</p></div>
 		<div class="bar" style="width: 20%;"><p>Editar mensaje</p></div>
 	</div>
 </div>
