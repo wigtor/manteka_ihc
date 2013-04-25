19cc4f1102efdd3a9bae7573ec29e49f1a94b369
Mario <marchim12@hotmail.com>
2013-04-25 15:13:21 -0300

    avances en la parte de javascript

--- CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
@@ -14,13 +14,14 @@
 
 
 ?>
+
 <fieldset>
     <legend>Secciones</legend>
     <div class="row"><!--fila-->
         <div class="span3">
             <h4>Escoja una seccion de la lista</h4>
             <input type="text" placeholder="Filtro búsqueda"><br/>
-            <select size=16>
+            <select id="select-secciones" size=16 onchange="mostrarDatos(this)">
                 <?php
                     foreach ($listado_secciones as $seccion) {
                         echo "<option value='".$seccion["id"]."'>".$seccion['nombre']."</option>";
@@ -30,10 +31,10 @@
             </select>
         </div>
         <h4>Vea la informacion de la seccion seleccionada</h4>
-        <h6>Profesor: <span id="profesor"> <!-- inyectar valor aqui --> </span></h6>
-        <h6>Modulo:   <span id="modulo">   <!-- inyectar valor aqui --> </span></h6>
-        <h6>Horario:  <span id ="horario"> <!-- inyectar valor aqui --> </span></h6>
-        <h6>Sala:     <span id="sala">     <!-- inyectar valor aqui --> </span></h6>
+        <h6>Profesor: <span id="mostrar-profesor"> <!-- inyectar valor aqui --> </span></h6>
+        <h6>Modulo:   <span id="mostrar-modulo">   <!-- inyectar valor aqui --> </span></h6>
+        <h6>Horario:  <span id="mostrar-horario">  <!-- inyectar valor aqui --> </span></h6>
+        <h6>Sala:     <span id="mostrar-sala">     <!-- inyectar valor aqui --> </span></h6>
         <div class="span9" style="overflow:auto; height:200px">
             <table class="table table-bordered" >            
                 <tr>
@@ -44,7 +45,7 @@
                     <th>Nombres</th>
                     <th>Mail</th>
                 </tr>
-                <span id="tabla_alumnos">
+                <span id="mostrar-tabla_alumnos">
                     <!-- aqui se inyectará con javascript la lista de alumnos de la seccion elegida -->
                 </span>
                 
@@ -54,3 +55,21 @@
     </div>
 </br>
 <fieldset>
+
+<script type="text/javascript">
+    
+    var array = <?php json_encode($listado_secciones); ?>;
+    //alert(array[0]);
+    function mostrarDatos(seleccion){
+        //borrar datos de los spans
+        $("#mostrar-profesor,#mostrar-modulo, #mostrar-horario, #mostrar-sala, #mostrar-tabla_alumnos").empty();
+        //setear nuevos datos de los spans
+        //$("#mostrar-profesor,#mostrar-modulo, #mostrar-horario, #mostrar-sala, #mostrar-tabla_alumnos").append("asddfsg");
+        //var id_seccion = $("#select-secciones").value();
+        var id_seccion = seleccion.options[seleccion.selectedIndex].value;
+
+        //alert(<?php echo "'"; var_dump($listado_secciones);echo "'"; ?>)
+        //alert(<?php echo "'".$listado_secciones."'"; ?>);
+        
+    }
+</script>
