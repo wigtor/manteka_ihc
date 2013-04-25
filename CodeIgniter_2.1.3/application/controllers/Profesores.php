6520fa0b85ba7d1ac18bbf84cd2c17022577f8bd
Mario <marchim12@hotmail.com>
2013-04-25 15:16:22 -0300

    .

--- CodeIgniter_2.1.3/application/controllers/Profesores.php
+++ CodeIgniter_2.1.3/application/controllers/Profesores.php
@@ -61,8 +61,7 @@ class Profesores extends CI_Controller {
 		$datos_plantilla["mostrarBarraProgreso"] = FALSE; //Cambiar en caso que no se necesite la barra de progreso
 		$datos_plantilla["barra_progreso_atras_siguiente"] = $this->load->view('templates/barra_progreso_atras_siguiente', $datos_plantilla, true);
 		$datos_plantilla["footer"] = $this->load->view('templates/footer', '', true);
-		$this->load->model('model_profesor');
-		$listado_profesores['listado_profesores'] = $this->model_profesor->ObtenerProfesor();
+		
 		$datos_plantilla["cuerpo_central"] = $this->load->view('cuerpo_profesores_crear', $listado_profesores, true); //Esta es la linea que cambia por cada controlador
 		$datos_plantilla["barra_lateral"] = $this->load->view('templates/barras_laterales/barra_lateral_profesores', '', true); //Esta linea también cambia según la vista como la anterior
 		$this->load->view('templates/template_general', $datos_plantilla);
