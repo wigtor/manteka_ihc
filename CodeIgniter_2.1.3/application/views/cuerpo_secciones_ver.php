f9632be83b7df8e9c4ee6019781dc698a1b84649
Diego jaume <diego.jaume@usach.cl>
2013-04-25 18:46:24 -0400

    Se agrego el VER de secciones

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
@@ -0,0 +1,35 @@
+<?php
+//crear un arreglo que tenga el nombre (listado_secciones) y ponerle datos random para mostrar en las tablitas.
+    
+
+?>
+<fieldset>
+    <legend>Secciones</legend>
+    <h1>Escoja una seccion de la lista</h1>
+    <input type="text" placeholder="Filtro búsqueda"><br/>
+    <select multiple="multiple">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+    </select>
+    <table class="table table-bordered">
+        <tr>
+            <th>Carrera</th>
+            <th>Rut</th>
+            <th>Paterno</th>
+            <th>Materno</th>
+            <th>Nombres</th>
+            <th>Mail</th>
+        </tr>
+        <tr>
+            <td>Ing</td>
+            <td>17444444-2</td>
+            <td>palomino</td>
+            <td>Orellana</td>
+            <td>Jose</td>
+            <td>adsads@usach.cl</td>
+        </tr>
+    </table>
+<fieldset>
