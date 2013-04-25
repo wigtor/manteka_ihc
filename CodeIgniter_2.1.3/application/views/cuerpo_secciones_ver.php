7f97fa17ba5385241bcc310aa0740e4ced9fd360
Mario <marchim12@hotmail.com>
2013-04-25 12:53:18 -0300

    vista ver secciones, lista para recibir datos [sin mostrar selección aún]

--- CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
@@ -1,6 +1,18 @@
 <?php
-    $arreglito1 = array("ingenieria",12558996-8,"araucano","maternox","gonzalo","miMail@usach.cl");
-    $arreglito2 = array("Arquitectura",1111111-8,"muñoz","varas","lokohanks","miMail@usach.cl");
+
+    $alumnos=[["ingenieria",12558996-8,"araucano","maternox","gonzalo","miMail@usach.cl"],["Arquitectura",1111111-8,"muñoz","varas","lokohanks","miMail@usach.cl"]];
+    
+    $seccion1 = ['id'=>1, 'nombre'=>"nombreseccion", "modulo"=>"M3", "Horario"=>"9:30","sala"=>"565","alumnos"=>$alumnos];
+    $seccion2 = ['id'=>2, 'nombre'=>"nombreseccio2", "modulo"=>"X3", "Horario"=>"9:3__0","sala"=>"5653","alumnos"=>$alumnos];
+    $seccion3 = ['id'=>3, 'nombre'=>"nombreseccio3", "modulo"=>"v3", "Horario"=>"9:_30","sala"=>"56567","alumnos"=>$alumnos];
+    $listado_secciones[$seccion1['id']] = $seccion1;
+    $listado_secciones[$seccion2['id']] = $seccion2;
+    $listado_secciones[$seccion3['id']] = $seccion3;
+    //var_dump( $listado_secciones);
+
+                    
+
+
 ?>
 <fieldset>
     <legend>Secciones</legend>
@@ -8,20 +20,21 @@
         <div class="span3">
             <h4>Escoja una seccion de la lista</h4>
             <input type="text" placeholder="Filtro búsqueda"><br/>
-            <select multiple="multiple" style="height:280px">
-                <option><?php echo "{$secciones[0]}" ?></option>
-                <option><?php echo "{$secciones[1]}" ?></option>
-                <option><?php echo "{$secciones[2]}" ?></option>
-                <option><?php echo "{$secciones[3]}" ?></option>
-                <option><?php echo "{$secciones[4]}" ?></option>
+            <select size=16>
+                <?php
+                    foreach ($listado_secciones as $seccion) {
+                        echo "<option value='".$seccion["id"]."'>".$seccion['nombre']."</option>";
+                    }
+                ?>
+                
             </select>
         </div>
-            <h4>Vea la informacion de la seccion seleccionada</h4>
-            <h6>Profesor: Juan Jota</h6>
-            <h6>Modulo: M2</h6>
-            <h6>Horario: M3</h6>
-            <h6>Sala: 565</h6>
-            <div class="span9" style="overflow:auto; height:200px">
+        <h4>Vea la informacion de la seccion seleccionada</h4>
+        <h6>Profesor: <span id="profesor"> <!-- inyectar valor aqui --> </span></h6>
+        <h6>Modulo:   <span id="modulo">   <!-- inyectar valor aqui --> </span></h6>
+        <h6>Horario:  <span id ="horario"> <!-- inyectar valor aqui --> </span></h6>
+        <h6>Sala:     <span id="sala">     <!-- inyectar valor aqui --> </span></h6>
+        <div class="span9" style="overflow:auto; height:200px">
             <table class="table table-bordered" >            
                 <tr>
                     <th>Carrera</th>
@@ -31,54 +44,11 @@
                     <th>Nombres</th>
                     <th>Mail</th>
                 </tr>
-                <tr>
-                    <td><?php echo "{$arreglito1[0]}" ?></td>
-                    <td><?php echo "{$arreglito1[1]}" ?></td>
-                    <td><?php echo "{$arreglito1[2]}" ?></td>
-                    <td><?php echo "{$arreglito1[3]}" ?></td>
-                    <td><?php echo "{$arreglito1[4]}" ?></td>
-                    <td><?php echo "{$arreglito1[5]}" ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "{$arreglito2[0]}" ?></td>
-                    <td><?php echo "{$arreglito2[1]}" ?></td>
-                    <td><?php echo "{$arreglito2[2]}" ?></td>
-                    <td><?php echo "{$arreglito2[3]}" ?></td>
-                    <td><?php echo "{$arreglito2[4]}" ?></td>
-                    <td><?php echo "{$arreglito2[5]}" ?></td>
-                </tr>
-                 <tr>
-                    <td><?php echo "{$arreglito1[0]}" ?></td>
-                    <td><?php echo "{$arreglito1[1]}" ?></td>
-                    <td><?php echo "{$arreglito1[2]}" ?></td>
-                    <td><?php echo "{$arreglito1[3]}" ?></td>
-                    <td><?php echo "{$arreglito1[4]}" ?></td>
-                    <td><?php echo "{$arreglito1[5]}" ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "{$arreglito2[0]}" ?></td>
-                    <td><?php echo "{$arreglito2[1]}" ?></td>
-                    <td><?php echo "{$arreglito2[2]}" ?></td>
-                    <td><?php echo "{$arreglito2[3]}" ?></td>
-                    <td><?php echo "{$arreglito2[4]}" ?></td>
-                    <td><?php echo "{$arreglito2[5]}" ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "{$arreglito1[0]}" ?></td>
-                    <td><?php echo "{$arreglito1[1]}" ?></td>
-                    <td><?php echo "{$arreglito1[2]}" ?></td>
-                    <td><?php echo "{$arreglito1[3]}" ?></td>
-                    <td><?php echo "{$arreglito1[4]}" ?></td>
-                    <td><?php echo "{$arreglito1[5]}" ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "{$arreglito2[0]}" ?></td>
-                    <td><?php echo "{$arreglito2[1]}" ?></td>
-                    <td><?php echo "{$arreglito2[2]}" ?></td>
-                    <td><?php echo "{$arreglito2[3]}" ?></td>
-                    <td><?php echo "{$arreglito2[4]}" ?></td>
-                    <td><?php echo "{$arreglito2[5]}" ?></td>
-                </tr>
+                <span id="tabla_alumnos">
+                    <!-- aqui se inyectará con javascript la lista de alumnos de la seccion elegida -->
+                </span>
+                
+                
             </table>
         </div>
     </div>
