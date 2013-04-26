fa9ade72d4614c125a20b5ce653f0d914bec5e36
Vicente Bravo <vicenteb90@hotmail.com>
2013-04-26 11:39:40 +0200

    ver alumnos ..para todos los efectos ..perdon XD

--- CodeIgniter_2.1.3/application/views/cuerpo_alumnos_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_alumnos_ver.php
@@ -1,6 +1,43 @@
 
 <fieldset>
-	<legend>Cuerpo central</legend>
-	Hola mundo, este es el formulario que se muestra al cliente<br>
-	La barra y los botones que aparecen abajo se activan sólo si es necesario
+	<legend>Ver alumnos</legend>
+	<div class="span4">
+		<h4>Listado alumnos<h4/><br/>
+		<div class="input-append">
+			<input class="span11" id="appendedDropdownButton" type="text" placeholder="Filtro">
+			<div class="btn-group">
+				<button class="btn dropdown-toggle" data-toggle="dropdown">
+					Filtrar por
+					<span class="caret"></span>
+				</button>
+				<ul class="dropdown-menu">
+				<li>Rut</li>
+				<li>Nombre</li>
+				<li>Apellido paterno</li>
+				<li>Apellido materno</li>
+				<li>Correo</li>
+				<li>Carrera</li>
+				<li>Sección</li>
+				</ul>
+			</div>
+		</div>
+	    <select multiple="multiple" style="width:342px; height:170px">
+		<option>Me encantaria </option>
+		<option>saber</option>
+		<option>como hacer</option>
+		<option>esto pa que quede listo pal marito :( </option>
+        </select>
+	</div>
+	<div class="span1">
+	</div>
+	<div class="span6">
+		<h4>Detalle alumno</h4><br/>
+		<h6>Rut: 24500-03</h6>
+		<h6>Nombre: Don </h6>
+        <h6>Apellido paterno: Corleone</h6>
+	    <h6>Apellido materno: Ayer</h6>
+        <h6>Correo: instale@windows8.com</h6>
+		<h6>Carrera: No se ocupar</h6>
+		<h6>Sección: na-da</h6>
+	</div>
 </fieldset>
