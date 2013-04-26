08d1ae5e22a70e3636aeee0f0d492c87fd51d25f
Diego Berríos <ffafdie_go@hotmail.com>
2013-04-25 23:01:05 -0400

    Moldelo profesor y Controlador listos para mostrar por vista

--- CodeIgniter_2.1.3/application/views/cuerpo_profesores_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_profesores_ver.php
@@ -1,6 +1,8 @@
-
 <fieldset>
 	<legend>Cuerpo central</legend>
-	Hola mundo, este es el formulario que se muestra al cliente<br>
-	La barra y los botones que aparecen abajo se activan sólo si es necesario
+	
+	<?php
+		echo $listado_profesores[0]['NOMBRE_PROFESOR'];
+        
+		?>
 </fieldset>
