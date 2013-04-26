7f097403db9a737bc1e7f4db2d27d3d2c94a10b6
Marco Acevedo <marco.acevedo@usach.cl>
2013-04-25 23:27:33 -0300

    Integracion del modelo estudiantes
    Se agregó el modelo estudiantes a la carpeta de modelos

---
+++ CodeIgniter_2.1.3/application/models/model_estudiante.php
@@ -0,0 +1,36 @@
+<?php
+ 
+class Estudiante extends CI_Model {
+    int  rut_estudiante = '';
+    var nombre1_estudiante = '';
+    var nombre2_estudiante  = '';
+    var apellido_paterno='';
+    var apellido_materno='';
+    var correo_estudiante='';
+    var cod_seccion='';
+    var cod_carrera='';
+ 
+// Modelo para insertar y eliminar un estudiante. No se si está completamente correcto la verdad, no lo he podido probar
+ 
+    public function InsertarEstudiante() {
+        $this->rut_estudiante = $_POST['rut_estudiante'];
+        $this->nombre1_estudiante = $_POST['nombre1_estudiante'];
+        $this->nombre2_estudiante = $_POST['nombre2_estudiante'];
+        $this->apellido_paterno = $_POST['apellido_paterno'];
+        $this->apellido_materno = $_POST['apellido_materno'];
+        $this->correo_estudiante = $_POST['correo_estudiante'];
+        $this->cod_seccion = $_POST['cod_seccion'];
+        $this->cod_carrera = $_POST['cod_carrera'];
+
+
+        // inserta los campos guardados en la tabla estudiante
+        $this->db->insert('ESTUDIANTE', $this);
+    }
+
+    public function EliminarEstudiante($rut_estudiante)
+    {
+        $db->query("DELETE FROM ESTUDIANTE WHERE rut_estudiante = '$rut_estudiante' ");
+    }
+}
+ 
+?>
