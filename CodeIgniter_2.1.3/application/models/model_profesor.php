07161cab1c62a10262208e772edb4836876862e5
Marco Acevedo <marco.acevedo@usach.cl>
2013-04-27 17:58:37 -0300

    Avance vista borrar alumnos y modelo profesor
    Creación del modelo profesor y la vista de eliminar alumno

---
+++ CodeIgniter_2.1.3/application/models/model_profesor.php
@@ -0,0 +1,12 @@
+<?php
+class Profesor extends CI_Model {
+	int  rut_profesor = '';
+    var nombre_profesor = '';
+    var apellido_paterno='';
+    var correo_prof1='';
+    var correo_prof2=''
+    var telefono
+    var tipo=''; // se refiere a si es profesor normal o coordinador
+    
+}
+?>
--- CodeIgniter_2.1.3/application/views/cuerpo_alumnos_borrar.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_alumnos_borrar.php
@@ -1,5 +1,62 @@
-<fieldset>
-	<legend>Borrar Alumno</legend>
-	Hola mundo, este es el formulario que se muestra al cliente<br>
-	La barra y los botones que aparecen abajo se activan sólo si es necesario
-</fieldset>
+<div class= "row-fluid">
+	<div class= "span10">
+		<fieldset>
+			<div>
+			<legend>Borrar Alumno</legend>
+			</div>
+			<div>
+				<div class= "row-fluid">
+					
+					<div class= "span6">
+						<div style="margin-bottom:2%">
+							1.-Seleccionar Alumno:
+						</div>
+						<div class="row-fluid">
+							<div class="span6">
+								<div class="controls">
+		    						<input type="text" id="inputInfo" placeholder="Filtro">
+		  						</div>
+							</div>
+							<div class="span6">
+								<div class="btn-group" >
+						  			<a class="btn ">Filtrar por:</a>
+						  			<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+						  			<ul class="dropdown-menu">
+						    		<li><a>Carrera</a></li>
+						    		<li><a>Seccion</a></li>
+						 			 </ul>
+								</div>
+							</div>
+						</div>
+						<div class="flow_fluid">
+							<div class="span9">
+								<textarea rows="8" readonly="readonly" style="width:100%; height:100%"></textarea>
+								
+							</div>
+						</div>	
+					</div>
+
+
+					<div class="span6">
+						<div style="margin-bottom:2%">
+							2.-Detalle del Alumno:
+						</div>
+						<div class="row-fluid">
+							<div class="span8" style="margin-top:9%">
+								<div class="controls">
+		    						<textarea rows="8" readonly="readonly" style="width:100%; height:100%";></textarea>
+		  						</div>
+							</div>
+							
+						</div>
+					</div>	
+				</div>
+
+			</div>
+	
+
+			
+		</fieldset>
+	</div>
+	</div>
+</div>
