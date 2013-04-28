251ba7f9a6ad345b486d39316d73f6e2c5e4b90a
Victor Flores <victor.floress@usach.cl>
2013-04-28 15:44:48 -0400

    Agregada vista y controlador para error 404
    Se muestra una página personalizada para el error 404 o "page not found"

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_not_found.php
@@ -0,0 +1,11 @@
+
+<fieldset>
+	<legend>Página no encontrada</legend>
+	Ha ocurrido el conocido por muchos error 404 <br>
+	Esto significa que no se ha encontrado la página que usted ha solicitado.<br>
+	Presione el botón "atrás" de su navegador web o reescriba la dirección del sitio que desea ver.
+	
+	<br><br>
+	Acá irá alguna imagen que represente este error de manera más intuitiva
+	
+</fieldset>
