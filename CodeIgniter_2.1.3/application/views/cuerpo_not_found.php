42f934a8173d243bcbc3b83c1092252860ba9a5b
Victor Flores <victor.floress@usach.cl>
2013-04-28 16:04:10 -0400

    Agregada imagen a pageNotFound

--- CodeIgniter_2.1.3/application/views/cuerpo_not_found.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_not_found.php
@@ -1,11 +1,10 @@
 
 <fieldset>
 	<legend>Página no encontrada</legend>
-	Ha ocurrido el conocido por muchos error 404 <br>
+	<img src="/<?php echo config_item('dir_alias') ?>/img/explosion.png" class="img-polaroid">
+	<br><br>Actualmente es muy probable que se deba a que el sitio está en construcción<br>
+	<!-- <div>Ha ocurrido el conocido por muchos error 404 <br>
 	Esto significa que no se ha encontrado la página que usted ha solicitado.<br>
 	Presione el botón "atrás" de su navegador web o reescriba la dirección del sitio que desea ver.
-	
-	<br><br>
-	Acá irá alguna imagen que represente este error de manera más intuitiva
-	
+	</div> -->
 </fieldset>
