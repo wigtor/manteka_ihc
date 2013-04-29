c1d84f3a1e8675b77785a2845ca81c63a4ebe550
cristian cornejo <cristian.cornejo@usach.cl>
2013-04-28 23:11:59 -0400

    se ve la lista de alumnos y el detalle
    falta lo de los fltros y sacar el botón para que se vea mejor

--- CodeIgniter_2.1.3/application/views/cuerpo_alumnos_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_alumnos_ver.php
@@ -23,34 +23,56 @@
 					<thead>
 						<tr>
 							<th style="text-align:center;">Nombre Completo</th>
+							
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td style="text-align:center;">Marco De Las Nieves Acevedo Subercaseaux</td>
-						</tr>
-						<tr>
-							<td style="text-align:center;">Marco HTML6 Acevedo</td>
-						</tr>
-						<tr>
-							<td style="text-align:center;">Marcopete Vodkamilo Acevedorada Yashao</td>
-						</tr>
-						<tr>
-							<td style="text-align:center;">Pete</td>
-						</tr>
+					
+						<?php
+						$contador=0;
+						while ($contador<count($rs_estudiantes)){
+							
+							echo '<form  type="post" action="VerPorBoton/'. $rs_estudiantes[$contador][0].'">';
+							echo '<tr>';
+							echo	'<td  style="text-align:center;">'. $rs_estudiantes[$contador][3].' '.$rs_estudiantes[$contador][4].' ' . $rs_estudiantes[$contador][1].' '.$rs_estudiantes[$contador][2].'<p></p><input type="submit" value="detalle" align="right"></input></td>';
+							//echo '<input type="hidden" name="seleccion1" id="tipo" value="'. $rs_articulos[$contador][0].'">';
+							//echo '<input type="submit" name="cualquierNombre"">Agregar alumno</input>';
+							echo '</tr>';
+							echo '</form>';
+							
+							$contador = $contador + 1;
+						}
+						?>
 					</tbody>
 				</table>
 			</div>
 		</div>
-		<div class="span6" style="margin-left: 2%;">
-			2.-Detalle Alumnos
-			<pre style="margin-top: 16%; margin-left: 3%;">Rut:
-Nombre:
-Apellido Paterno:
-Apellido Materno:
-Correo:
-Carrera:
-Seccion:</pre>
+		<div class="span6" style="margin-left: 2%;">		
+		2.-Detalle Alumnos:
+	    <pre style="margin-top: 16%; margin-left: 3%;">
+		<?php
+		if(!empty($rs_estudiante)){
+			echo "\nRut: ".$rs_estudiante[0]."\n";
+			echo "Nombre: ".$rs_estudiante[1]."\n";
+			echo "Apellido paterno: ".$rs_estudiante[3]."\n";
+			echo "Apellido materno: ".$rs_estudiante[4]."\n";
+			echo "Correo: ".$rs_estudiante[5]."\n";
+			echo "Carrera: ".$rs_estudiante[7]."\n";
+			echo "Sección: ".$rs_estudiante[6]."\n";
+		}
+		else {
+			echo "\nRut:\n";
+			echo "Nombre:\n";
+			echo "Apellido paterno:\n";
+			echo "Apellido materno:\n";
+			echo "Correo:\n";
+			echo "Carrera:\n";
+			echo "Sección:\n";
+		}
+		?>
+		
+		</pre>
+
 		</div>
 	</div>
 </fieldset>
