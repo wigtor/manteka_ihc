0144783d296a613d0a9ee126887a31c3e0ae12ff
Jose Luis <jose.varela@usach.cl>
2013-05-01 19:46:08 -0400

    Funcion para signIn through Google

--- CodeIgniter_2.1.3/application/controllers/php.php
+++ CodeIgniter_2.1.3/application/controllers/php.php
@@ -45,8 +45,60 @@ class Php extends CI_Controller {
 		$this->load->helper('url');
 		$this->session->unset_userdata('rut');
 		$this->session->unset_userdata('email');
-		$this->session->unset_userdata('loggued_in');
+    $this->session->unset_userdata('loggued_in');
 		redirect('/Login/', 'refresh'); //   Lo regresamos a la pantalla de login y pasamos como par?metro el mensaje de error a presentar en pantalla
    }
+
+   /*
+   *  Función para autentificarse en el sistema mediante una cuenta Google.
+   *  El Controlador solicita la autentificación a Google.
+   *  Una vez realizado esto, el usuario se autentifica normalmente.
+   */
+   function signInGoogle($provider){
+    $this -> load -> helper('url');
+    $this->load->library('session');
+    $this -> load -> spark('oauth2/0.4.0');
+   //si el proveedor pasado es foursquare
+    if($provider == 'foursquare')
+    {
+        $provider = $this -> oauth2 -> provider($provider, array(
+        'id' => 'tu_app_id_de_foursquare',
+        'secret' => 'tu_app_secret_de_foursquare', ));
+       //si el proveedor pasado es google
+    }else if($provider == 'google')
+    {
+        $provider = $this -> oauth2 -> provider($provider, array(
+      tu_app_id
+        'id' => 'tu_client_id_de_google',
+        'secret' => 'tu_client_secret_de_google', ));
+    }
+
+    if (!$this -> input -> get('code')) {
+        // By sending no options it'll come back here
+        $url = $provider -> authorize();
+
+        redirect($url);
+    } else {
+        try {
+            // Have a go at creating an access token from the code
+            $token = $provider -> access($this->input->get('code'));
+
+            // Use this object to try and get some user details (username, full name, etc)
+            $user = $provider->get_user_info($token); 
+
+            // Here you should use this information to A) look for a user B) help a new user sign up with existing data.
+            // If you store it all in a cookie and redirect to a registration page this is crazy-simple.
+            echo "<pre>Tokens: ";
+            var_dump($token);
+
+            echo "\n\nUser Info: ";
+            var_dump($user);
+        } catch (OAuth2_Exception $e) {
+            show_error('That didnt work: ' . $e);
+        }
+    }
+
+   }
+
 }
 ?>
