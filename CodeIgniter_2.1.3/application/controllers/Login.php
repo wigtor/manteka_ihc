0ec3f64c00eeba5b2a031bbc1717296367e619d5
marco baeza <marco.baeza@usach.cl>
2013-05-02 21:38:48 -0400

    Validaciones de login funcionales
    Además se ha eliminado php.php para funciones de login

--- CodeIgniter_2.1.3/application/controllers/Login.php
+++ CodeIgniter_2.1.3/application/controllers/Login.php
@@ -82,6 +82,39 @@ class Login extends CI_Controller {
 		}
 	}
 
+	public function LoginPost() {
+		$Rut = $this->input->post('inputRut');
+		$this->form_validation->set_rules('inputRut', 'usuario', "required|callback_check_userRUT");
+		$this->form_validation->set_rules('inputPassword', 'contraseña', "required|callback_check_user_and_password[$Rut]");
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->index(); //Vuelvo a llamar el cambio de contraseña si hubo un error
+		}else{
+			 $this->load->model('model_usuario');
+            $ExisteUsuarioyPassoword=$this->model_usuario->ValidarUsuario($_POST['inputRut'],$_POST['inputPassword']);   //   comprobamos que el usuario exista en la base de datos y la password ingresada sea correcta
+            if($ExisteUsuarioyPassoword){   // La variable $ExisteUsuarioyPassoword recibe valor TRUE si el usuario existe y FALSE en caso que no. Este valor lo determina el modelo.
+				      $newdata = array(
+                   'rut'  => $ExisteUsuarioyPassoword->RUT_USUARIO,
+                   'email'     => $ExisteUsuarioyPassoword->CORREO1_USER,
+                   'tipo_usuario' => $ExisteUsuarioyPassoword->ID_TIPO,
+                   'logged_in' => TRUE
+              );
+				      $this->session->set_userdata($newdata);
+				
+				      redirect('/Correo/', 'index');
+				    }
+            else{   //   Si no logró validar
+               //$data['error']="Rut o password incorrecta, por favor vuelva a intentar";
+		          $this->session->unset_userdata('rut');
+		          $this->session->unset_userdata('email');
+              $this->session->unset_userdata('tipo_usuario');
+		          $this->session->unset_userdata('loggued_in');
+              redirect('/Login/', 'refresh'); //   Lo regresamos a la pantalla de login y pasamos como par?metro el mensaje de error a presentar en pantalla
+            }
+		}
+	}
+
 	public function recuperaPassPost() {
 		$this->form_validation->set_rules('email', 'email', "required|email|xss_clean|callback_check_mail_exist");
 		if ($this->form_validation->run() == FALSE)
@@ -238,6 +271,114 @@ class Login extends CI_Controller {
 			return FALSE;
 		}
 	}
+
+	public function check_userRUT($user) {
+		$this->load->model('model_usuario');
+		$logueo = $this->model_usuario->ValidarRut($user);
+		if ($logueo) {
+			return TRUE;
+		}
+		else {
+			$this->form_validation->set_message('check_userRUT', 'El %s no esta en el sistema');
+			return FALSE;
+		}
+	}
+
+	function logout() {
+		$this->load->library('session');
+		$this->load->helper('url');
+		$this->session->unset_userdata('rut');
+		$this->session->unset_userdata('email');
+    $this->session->unset_userdata('loggued_in');
+		redirect('/Login/', 'refresh'); //   Lo regresamos a la pantalla de login y pasamos como par?metro el mensaje de error a presentar en pantalla
+   }
+
+
+      /*
+   *  Función para autentificarse en el sistema mediante una cuenta Google.
+   *  El Controlador solicita la autentificación a Google.
+   *  Una vez realizado esto, el usuario se autentifica normalmente.
+   */
+   function signInGoogle($provider){
+    $rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesión iniciada
+    if ($rut == TRUE) {
+      redirect('/Correo/', 'index');         // En dicho caso, se redirige a la interfaz principal
+    }
+
+    $this -> load -> helper('url');
+    $this->load->library('session');
+    $this -> load -> spark('oauth2/0.4.0');
+    //si el proveedor pasado es Google
+    if($provider == 'google')
+    {
+        $provider = $this -> oauth2 -> provider($provider, array(
+        'id' => '412900046548.apps.googleusercontent.com',
+        'secret' => 'RN_R-d6BDT2XYwQdVHB5S9tO', ));
+    }
+
+    if (!$this -> input -> get('code')) {
+        $url = $provider -> authorize();
+
+        redirect($url);
+    } else {
+        try {
+            // Se posee de un Token exitoso enviado por google
+            $token = $provider -> access($this->input->get('code'));
+
+            // Se obtiene la información del usuario (Nombre, mail, dirección, foto)
+            $user = $provider->get_user_info($token); 
+
+            // Correo del usuario ingresado
+            $mail = $user['email'];
+            
+            //Cargando modelo para validar correo del usuario ingresado con algún registro en la db
+            $this->load->model('model_usuario');
+            
+            /*
+              * Verificando si existe algún usuario con dicho correo electrónico. 
+              * Resultado de la consulta del modelo. Falso de no encontrar nada o
+              * Las filas correspondientes a los usuarios que posean dicho mail.
+            */
+            $usuario = $this->model_usuario->existe_mail($mail);
+            if ($usuario)
+            {
+              $newdata = array(
+                   'rut'  => $usuario->RUT_USUARIO,
+                   'email'     => $usuario->CORREO1_USER,
+                   'tipo_usuario' => $usuario->ID_TIPO,
+                   'logged_in' => TRUE
+              );
+              $this->session->set_userdata($newdata);
+              redirect('/Correo/', 'index');         // En dicho caso, se redirige a la interfaz principal
+            }
+            else // En caso de no existir ningún usuario con correo = $mail
+            {
+              $datos_plantilla["titulo_msj"] = "Error";
+              $datos_plantilla["cuerpo_msj"] = "El correo ingresado no se asocia a ningún usuario del sistema ManteKA";
+              $datos_plantilla["tipo_msj"] = "alert-error";
+
+              /* Finalmente muestro la vista que indica que esto fue realizado correctamente */
+              $datos_plantilla["title"] = "ManteKA";
+              $datos_plantilla["head"] = $this->load->view('templates/head', $datos_plantilla, true);
+              $datos_plantilla["banner_portada"] = $this->load->view('templates/banner_portada', '', true);
+              
+              $datos_plantilla["redirectAuto"] = FALSE; //Esto indica si por javascript se va a redireccionar luego de 5 segundos
+              $datos_plantilla["redirecTo"] = "Login/index"; //Acá se pone el controlador/metodo hacia donde se redireccionará
+              $datos_plantilla["redirecFrom"] = "Login/signInGoogle/google"; //Acá se pone el controlador/metodo desde donde se llegó acá, no hago esto si no quiero que el usuario vuelva
+              $datos_plantilla["nombre_redirecFrom"] = "Volver"; //Acá se pone el nombre del sitio hacia donde se va a redireccionar
+              $datos_plantilla["nombre_redirecTo"] = "Inicio de sesión"; //Acá se pone el nombre del sitio hacia donde se va a redireccionar
+              $this->load->view('templates/big_msj_deslogueado', $datos_plantilla);
+
+
+            }
+
+            
+        } catch (OAuth2_Exception $e) {
+            show_error('No se pudo loguear mediante Google :(: ' . $e);
+        }
+    }
+
+   }
 
 }
 
--- CodeIgniter_2.1.3/application/views/login.php
+++ CodeIgniter_2.1.3/application/views/login.php
@@ -19,20 +19,31 @@
 			</div>
 			<fieldset class="span3">
 				<legend>Inicio de sesión</legend>
-				<?php echo form_open('php/login/'); ?>
-						<div class="control-group">
+				<?php echo form_open('Login/LoginPost/'); ?>
+						<?php /* Con esto hago que cambie la class del control-group a 'error' en caso de que exista un error en la validación */
+							$inputRut = '';
+							$inputPassword = '';
+							if (form_error('inputPassword') != '') {
+								$inputPassword = 'error';
+							}
+							if (form_error('inputRut') != '') {
+								$inputRut = 'error';
+							}
+						?>
+						<div class="control-group <?php echo $inputRut ?>">
 							<label class="control-label" for="inputRut">Rut</label>
 							<div class="controls">
-							  <input style="width:200px" type="text" name="inputRut" id="inputRut" placeholder=" Ingrese rut, ejemplo: 1756574" value="<?= set_value('inputRut'); ?>">
-							  <STRONG>-</STRONG>
-							  <input style="width:15px" type="text" name="inputGuionRut" maxlength="1" id="inputGuionRut"  placeholder="K" value="<?= set_value('inputGuionRut'); ?>">
+							  	<input style="width:200px" type="text" name="inputRut" id="inputRut" placeholder=" Ingrese rut, ejemplo: 1756574" value="<?= set_value('inputRut'); ?>">
+							 	<STRONG>-</STRONG>
+							  	<input style="width:15px" type="text" name="inputGuionRut" maxlength="1" id="inputGuionRut"  placeholder="K" value="<?= set_value('inputGuionRut'); ?>">
+								<?= form_error('inputRut', '<span class="help-inline">', '</span>');?>
 							</div>
 						</div>
-						<div class="control-group">
+						<div class="control-group <?php echo $inputPassword ?>">
 							<label class="control-label" for="inputPassword">Contraseña</label>
 							<div class="controls">
 								<input style="width:242px" type="password" name="inputPassword" onclick="calculaDigitoVerificador()" id="inputPassword" placeholder="  Ingrese su contraseña" value="<?= set_value('inputPassword'); ?>">
-								<div class="LoginUsuariosError"><?= form_error('passwordlogin');?></div>
+								<?= form_error('inputPassword', '<span class="help-inline">', '</span>');?>
 							</div>
 						</div>
 						<div class="control-group">
@@ -48,7 +59,7 @@
 						</div>
 						<hr>
 				<?php echo form_close(""); ?>
-				<?php echo form_open('php/signInGoogle/google'); ?>
+				<?php echo form_open('Login/signInGoogle/google'); ?>
 						<div class="control-group text-center">
 							<button type="submit" class="btn">
 								<div class="pull-right">
