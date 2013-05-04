7899ff3ddb111ea3e23f3587f5dfa93509db4247
Diego Escobar Lagos <diego.escobarla@usach.cl>
2013-05-04 17:26:21 -0400

    Revert "borre unos archivos para cambiarlos"
    This reverts commit 2cfe3a217776ea25da19a094c42c1bbe1768c68b.

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_alumnos_agregar.php
@@ -0,0 +1,127 @@
+<div class= "row-fluid">
+	<div class= "span10">	
+		<fieldset>	
+			<legend>Agregar Alumno</legend>
+			<div>
+				<div class= "row-fluid">
+					<div class= "span6" style="margin-bottom:2%">
+						Ingrese datos del Alumno:
+					</div>
+				</div>
+				<div  class= "row-fluid" style="margin-left:2%">
+					<div class= "span6">
+						<div class="row">
+							<div class="span4">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">1-.*RUT</label>
+		  						</div>
+		  					</div>
+		  					<div class="span5">	
+		  							<div class="controls">
+		    							<input type="text" id="inputInfo">
+		  							</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="span4">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">2-.*Nombres</label>
+		  						</div>
+		  					</div>
+		  					<div class="span5">	
+		  							<div class="controls">
+		    							<input type="text" id="inputInfo">
+		  							</div>
+							</div>
+
+						</div>
+						<div class="row">
+							<div class="span4">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">3-.*Apellido Paterno</label>
+		  						</div>
+		  					</div>
+		  					<div class="span5">	
+		  							<div class="controls">
+		    							<input type="text" id="inputInfo">
+		  							</div>
+							</div>
+
+						</div>
+						<div class="row">
+							<div class="span4">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">4-.*Apellido Materno</label>
+		  						</div>
+		  					</div>
+		  					<div class="span5">	
+		  							<div class="controls">
+		    							<input type="text" id="inputInfo">
+		  							</div>
+							</div>
+
+						</div>
+						<div class="row">
+							<div class="span4">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">5-.*Correo</label>
+		  						</div>
+		  					</div>
+		  					<div class="span5">	
+		  							<div class="controls">
+		    							<input type="text" id="inputInfo">
+		  							</div>
+							</div>
+
+						</div>
+
+					</div> 
+
+					<div class="span6" >
+						<div class="row">
+							<div class="span5">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">6-.*Asignar Carrera</label>
+		  						</div>
+							</div>
+							<div  class="span6">
+								<div class="controls">
+		    						<input type="text" id="inputInfo" placeholder="Filtro de Carrera">
+		  						</div>
+		  						<div>
+		  							<textarea rows="3" readonly="readonly"></textarea>
+		  						</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="span5">
+								<div class="control-group">
+		  							<label class="control-label" for="inputInfo">7-.*Asignar sección</label>
+		  						</div>
+							</div>
+							<div  class="span6">
+								<div class="controls">
+		    						<input type="text" id="inputInfo" placeholder="Filtro de Sección">
+		  						</div>
+		  						<div>
+		  							<textarea rows="3" readonly="readonly"></textarea>
+		  						</div>
+							</div>
+
+						</div>
+						
+					</div>
+				</div>
+				<div class="row" style="width: 845px;">		
+						<div class="span2" style="margin-left: 668px;">
+								<button class="btn" type="submit">Agregar</button>
+							</div>
+							<div class="span1" style="margin-left: -32px;">
+								<button class="btn" type="reset">Cancelar</button>
+							</div>
+						</div>
+					</div> 
+			</div>
+		</fieldset>
+	</div>
+</div>
