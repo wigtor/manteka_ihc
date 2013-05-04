e7ba90caffea77fdd409d6c88123dbd861226aaa
Marco Acevedo <marco.acevedo@usach.cl>
2013-05-03 23:00:07 -0400

    comienzo de cambios en agregar secciones
    se implementan los cambios que alcance antes de clases

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_agregar.php
@@ -0,0 +1,49 @@
+<div class="row-fluid">
+	<div class= "span10">
+		<fieldset>
+			<div>
+				<legend>Agregar Sección</legend>
+			</div>
+			<div class="row-fluid">
+				<div class="span6">
+					<div class="row-fluid">
+						<div class="span5">
+							1.-*Número de la Sección
+						</div>
+						<div class="span5">
+							<input type="text" id="info" class="span2">
+							<input type="text" id="info" class="span2">
+						</div>
+
+					</div>
+					<div class="row-fluid">
+						<div class="span5">
+							2.-Agregar estudiantes
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div class="span7">
+							<input type="text" id="info" class="span13" placeholder="Filtro búsqueda">
+						</div>
+						<div class="span5">
+							<select class="span13">
+								  <option>1</option>
+								  <option>2</option>
+								  <option>3</option>
+								  <option>4</option>
+								  <option>5</option>
+							</select>
+
+						</div>
+
+					</div>
+
+					<div class="span13">
+					</div>
+
+				</div>
+			</div>
+			
+		</fieldset>
+	</div>
+</div>
