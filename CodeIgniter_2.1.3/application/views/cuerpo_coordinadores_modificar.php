08d8d6f6b39050e86eae288b88cfa3c4dee945ab
Vicente Bravo <vicenteb90@hotmail.com>
2013-05-05 11:55:43 +0200

    Mis 4 vistitas
    Hay una que le falta un detalle pero lo hare llegando en la noche ..es
    solo un icono que no me resulto ..les amo

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_coordinadores_modificar.php
@@ -0,0 +1,77 @@
+<fieldset>
+	<legend>Modificar Coordinador</legend>
+	<div class="span4">
+		<h4>Seleccione un coordinador a modificar:<h4/><br/>
+		<div class="input-append">
+			<input class="span11" id="appendedDropdownButton" type="text" placeholder="Filtro">
+			<div class="btn-group">
+				<button class="btn dropdown-toggle" data-toggle="dropdown">
+					Filtrar por
+					<span class="caret"></span>
+				</button>
+				<ul class="dropdown-menu">
+				<li>Nombre</li>
+				<li>Rut</li>
+				<li>Contraseña</li>
+				<li>Reingresar contraseña</li>
+				<li>Correo </li>
+				<li>Teléfono</li>
+				</ul>
+			</div>
+		</div>
+	    <select size=18 style="width:342px">
+		<option>Coordinador 1</option>
+		<option>Coordinador 2</option>
+		<option>Coordinador 3</option>
+		<option>Coordinador 4</option>
+        </select>
+	</div>
+	<div class="span6 offset1">
+		<div class="span12">
+			<h4>Complete los siguientes datos para modificar un coordinador:</h4><br/>
+			<div class="span9">
+				<br/>
+				<table>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Nombre completo  :</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="ej:SOLAR FUENTES MAURICIO IGNACIO"></td>
+				</tr>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Rut :</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="ej:5946896-3"></td>
+				</tr>			
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Contraseña :</h6></td>
+				<td><input class ="input-xlarge"  type="text" placeholder="*******"></td>
+				</tr>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Reingresar contraseña:</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="*******"></td>
+				</tr>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Correo 1 :</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="ej:edmundo.leiva@usach.cl"></td>
+				</tr>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Correo 2 :</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="edmundo@gmail.com"></td>
+				</tr>
+				<tr>
+				<td><h6><span class="text-error">(*)</span>Teléfono :</h6></td>
+				<td><input class ="input-xlarge" type="text" placeholder="ej:9-87654321"></td>
+				</tr>
+				<tr>
+				<td></td>
+				<td>Los campos con <span class="text-error">(*)</span> son obligatorios</td>
+				</tr>
+				</table>
+				
+			</div>
+		</div>	
+	</div>
+	<div class="span3 offset9">
+		<button class="btn" type="button">Guardar</button>
+		<button class="btn" type="button">Cancelar</button>
+	</div>
+	
+</fieldset>
