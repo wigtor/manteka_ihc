146ae081006444fb4921f13cee34098a685934a7
Mario <marchim12@hotmail.com>
2013-05-06 10:05:33 -0300

    vistas coordinadores ver y modificar terminadas

--- CodeIgniter_2.1.3/application/views/cuerpo_coordinadores_modificar.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_coordinadores_modificar.php
@@ -34,16 +34,16 @@
 				foreach ($listado_coordinadores as $coordinador){
 					echo "<form class='span9' id='id".$coordinador['id']."' method='POST' action='/manteka/index.php/Coordinadores/modificarCoordinador/'>";
 					echo "<br/><table>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Nombre completo :</h6></td><td><input name='nombre' class ='input-xlarge' type='text' placeholder='ej:SOLAR FUENTES MAURICIO IGNACIO' default='".$coordinador['nombre']."'></td></tr>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Rut :</h6></td><td><input name='rut' class ='input-xlarge' type='text' placeholder='ej:5946896-3' default='".$coordinador['rut']."'></td></tr>	";		
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Contraseña :</h6></td><td><input name='contrasena' class ='input-xlarge'  type='text' placeholder='*******' default='".$coordinador['contrasena']."'></td></tr>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Reingresar contraseña:</h6></td><td><input name='contrasena2' class ='input-xlarge' type='text' placeholder='*******' default='".$coordinador['contrasena']."'></td></tr>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Correo 1 :</h6></td><td><input name='correo1' class ='input-xlarge' type='text' placeholder='ej:edmundo.leiva@usach.cl' default='".$coordinador['correo1']."'></td></tr>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Correo 2 :</h6></td><td><input name='correo2' class ='input-xlarge' type='text' placeholder='ej:edmundo@gmail.com' default='".$coordinador['correo2']."'></td></tr>";
-					echo "<tr><td><h6><span class='text-error'>(*)</span>Teléfono :</h6></td><td><input name='fono' class ='input-xlarge' type='text' placeholder='ej:9-87654321' default='".$coordinador['fono']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Nombre completo :</h6></td><td><input name='nombre' class ='input-xlarge' type='text' placeholder='ej:SOLAR FUENTES MAURICIO IGNACIO' default='".$coordinador['nombre']."' value='".$coordinador['nombre']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Rut :</h6></td><td><input name='rut' class ='input-xlarge' type='text' placeholder='ej:5946896-3' default='".$coordinador['rut']."' value='".$coordinador['rut']."'></td></tr>	";		
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Contraseña :</h6></td><td><input name='contrasena' class ='input-xlarge'  type='text' placeholder='*******' default='".$coordinador['contrasena']."' value='".$coordinador['contrasena']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Reingresar contraseña:</h6></td><td><input name='contrasena2' class ='input-xlarge' type='text' placeholder='*******' default='".$coordinador['contrasena']."' value='".$coordinador['contrasena']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Correo 1 :</h6></td><td><input name='correo1' class ='input-xlarge' type='text' placeholder='ej:edmundo.leiva@usach.cl' default='".$coordinador['correo1']."' value='".$coordinador['correo1']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Correo 2 :</h6></td><td><input name='correo2' class ='input-xlarge' type='text' placeholder='ej:edmundo@gmail.com' default='".$coordinador['correo2']."' value='".$coordinador['correo2']."'></td></tr>";
+					echo "<tr><td><h6><span class='text-error'>(*)</span>Teléfono :</h6></td><td><input name='fono' class ='input-xlarge' type='text' placeholder='ej:9-87654321' default='".$coordinador['fono']."' value='".$coordinador['fono']."'></td></tr>";
 					echo "<tr><td></td><td>Los campos con <span class='text-error'>(*)</span> son obligatorios</td></tr>";
 					echo "</table>";
-					echo "<div class='span6 offset5' id='botones-guardar-cancelar'><button type='submit' class='btn' type='button'>Guardar</button><button class='btn offset1' type='button'>Cancelar</button></div>";
+					echo "<div class='span6 offset5' id='botones-guardar-cancelar'><button type='submit' class='btn' type='button'>Guardar</button><a class='btn offset1' href='/manteka/index.php/Coordinadores/modificarCoordinador/'>Cancelar</a></div>";
 					echo "</form><!-- span9-->";
 				}
 				
@@ -67,11 +67,10 @@
 
         $("#"+id_coordinador).show();
 
-        var inputs = $("#"+id_coordinador+" input");
-        //alert(inputs.first().attr('default'));
+        /*var inputs = $("#"+id_coordinador+" input");
         for (var i in inputs) {
-        	alert(i.attr("default"))
-        }
+        	i.value= i.attr("default");
+        }*/
     }
        	
     	
--- CodeIgniter_2.1.3/application/views/cuerpo_coordinadores_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_coordinadores_ver.php
@@ -4,7 +4,7 @@
 	    <div class="row"><!--fila-->
 	        <div class="span3">
 	            <h4>1.- Lista Coordinadores</h4>
-	            <input class="span6" type="text" placeholder="Filtro búsqueda">
+	            <input class="span8" type="text" placeholder="Filtro búsqueda">
 	            <select class="span4">
 				    <option>Filtrar Por...</option>
 				    <option>#</option>
@@ -12,30 +12,48 @@
 				    <option>#</option>
 				    <option>#</option>
 				</select>
-	            <select id="select-coordinadores" size=16 onchange="mostrarDatos(this)">
+	            <select id="select-coordinadores" class="span12" size=16 onchange="mostrarDatos(this)">
 	            <?php
 	                foreach ($listado_coordinadores as $coordinador) {
-	                    echo "<option value='".$coordinador["id"]."'>".$coordinador['nombre']."</option>";
+	                    echo "<option value='id".$coordinador["id"]."'>".$coordinador['nombre']."</option>";
 	                }
 	            ?>
 	            </select>
 	        </div>
-	        <div class="span7">
-	        <h4>Detalle Coordinador</h4><br />
-	        
-	            <h4>Coordinador: Solar Fuentes Mauricio Gonzalo</h4>
-	            <h4>Rut: 17555888-4</h4>
-	            <h4>Mail: asd@fasd.com</h4>
-	            <h4>Fono: 027378899</h4>
-	            <h4>Modulo: Comunicacion y compromiso</h4>
-	            <h4>Seccion: H1,H2</h4>
-
-	                <span id="mostrar-tabla_alumnos">
-	                    <!-- aqui se inyectará con javascript la lista de alumnos de la seccion elegida -->
-	                </span>
-
-	        </div>
-	        <div class="span1"></div>
+	        <?php 
+	        	foreach ($listado_coordinadores as $coordinador) {
+		        	echo "<div class='span7 offset1 visualizar-coordinador' id='id".$coordinador['id']."'>
+			            <h4>Detalle Coordinador</h4><br />
+			        
+			            <h4>Coordinador: ".$coordinador['nombre']."</h4>
+			            <h4>Rut: ".$coordinador['rut']."</h4>
+			            <h4>Mail: ".$coordinador['correo1']."</h4>
+			            <h4>Fono: ".$coordinador['fono']."</h4>
+			            <h4>Modulo: ".$coordinador['modulos']."</h4>
+			            <h4>Seccion: ".$coordinador['secciones']."</h4>
+			        </div>
+			        ";
+			    }
+	        ?>
 	    </div>
 	</br>
 </fieldset>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+    	$(".visualizar-coordinador").hide();
+
+    });
+	function mostrarDatos(seleccion){
+
+        
+        var id_coordinador = seleccion.options[seleccion.selectedIndex].value;
+        $(".visualizar-coordinador").hide();
+
+        $("#"+id_coordinador).show();
+    }
+       	
+    	
+
+
+</script>
