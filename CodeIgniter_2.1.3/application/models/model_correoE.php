be546cdd2c0052bbb1b70805612629857df7fb7f
byron lanas <byron.lanas@usach.cl>
2013-05-10 16:26:39 -0400

    documentación
    documentación de los modelos model_correoA, model_correoE, model_correoU
    y la vista cuerpo_correos_enviar

--- CodeIgniter_2.1.3/application/models/model_correoE.php
+++ CodeIgniter_2.1.3/application/models/model_correoE.php
@@ -1,6 +1,15 @@
 <?php
  
 class model_correoE extends CI_Model {
+
+   /**
+   *  Identifica si en la base de datos existe un usuario con una contraseña identica a la ingresada como parámetro.
+   *  En caso de validar al usuario, se retornan sus datos.
+   *  Caso contrario se retorna un arreglo nulo
+   *
+   *  @param string $rutRecept rut del estudiante que recibe el mensaje
+   *  @param string $date codigo del correo que se obtiene dependiendo de la fecha y hora
+   */
         public function InsertarCorreoE($rutRecept,$date){
         $this->COD_CORREO=$date ;
         $this->RUT_ESTUDIANTE=$rutRecept ;
--- CodeIgniter_2.1.3/application/models/model_correoU.php
+++ CodeIgniter_2.1.3/application/models/model_correoU.php
@@ -1,6 +1,16 @@
 <?php
  
 class model_correoU extends CI_Model {
+
+	/**
+   *  Identifica si en la base de datos existe un usuario con una contraseña identica a la ingresada como parámetro.
+   *  En caso de validar al usuario, se retornan sus datos.
+   *  Caso contrario se retorna un arreglo nulo
+   *
+   *  @param string $rutRecept del usuario recibe el mensaje
+   *  @param string $date codigo del correo que se obtiene dependiendo de la fecha y hora
+   */
+
         public function InsertarCorreoU($rutRecept,$date){
         $this->COD_CORREO=$date;
         $this->RUT_ESTUDIANTE=$rutRecept ;
