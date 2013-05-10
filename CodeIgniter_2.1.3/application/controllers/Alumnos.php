6f3820dec5ccbbe6c8f1bcf5a2c6988bfd8e56fc
cristian cornejo <cristian.cornejo@usach.cl>
2013-05-10 11:58:53 -0400

    comentarios en controlador alumno

--- CodeIgniter_2.1.3/application/controllers/Alumnos.php
+++ CodeIgniter_2.1.3/application/controllers/Alumnos.php
@@ -17,6 +17,20 @@ class Alumnos extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+	 
+	 
+	 
+	 
+	 
+	/**
+	* Manda a la vista 'cuerpo_alumnos_ver' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se cargan los datos de la vista con la lista 'rs_estudiantes' que contiene toda la información de los
+	* estudiantes del sistema y la lista 'carreras' que contiene información de las carreras en el sistema. Finalmente se carga la vista con todos los datos.
+	*
+	*/
 	public function verAlumnos()
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -49,7 +63,61 @@ class Alumnos extends CI_Controller {
 		$this->load->view('templates/template_general', $datos_plantilla);
 
 	}
+	
+	
+	/**
+	* Manda a la vista 'cuerpo_alumnos_borrar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se cargan los datos de la vista con la lista 'rs_estudiantes' que contiene toda la información de los
+	* estudiantes del sistema y se envia un 'mensaje_confirmacion' que sirve en la vista para que ésta sepa que se está cargando la página por primera vez y no que hace poco se haya borrado algún alumno
+	* Finalmente se carga la vista con todos los datos.
+	*
+	*/
+	public function borrarAlumnos()//carga la vista para borrar alumnos
+	{
+		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
+		if ($rut == FALSE) {
+			redirect('/Login/', ''); //Se redirecciona a login si no tiene sesi?n iniciada
+		}
+		$datos_plantilla["rut_usuario"] = $this->session->userdata('rut');
+		$datos_plantilla["nombre_usuario"] = $this->session->userdata('nombre_usuario');
+		$datos_plantilla["tipo_usuario"] = $this->session->userdata('tipo_usuario');
+		$datos_plantilla["title"] = "ManteKA";
+		$datos_plantilla["menuSuperiorAbierto"] = "Alumnos";
+		$datos_plantilla["head"] = $this->load->view('templates/head', $datos_plantilla, true);
+		$datos_plantilla["barra_usuario"] = $this->load->view('templates/barra_usuario', $datos_plantilla, true);
+		$datos_plantilla["banner_portada"] = $this->load->view('templates/banner_portada', '', true);
+		$datos_plantilla["menu_superior"] = $this->load->view('templates/menu_superior', $datos_plantilla, true);
+		$datos_plantilla["barra_navegacion"] = $this->load->view('templates/barra_navegacion', '', true);
+		$datos_plantilla["mostrarBarraProgreso"] = FALSE; //Cambiar en caso que no se necesite la barra de progreso
+		$datos_plantilla["barra_progreso_atras_siguiente"] = $this->load->view('templates/barra_progreso_atras_siguiente', $datos_plantilla, true);
+		$datos_plantilla["footer"] = $this->load->view('templates/footer', '', true);
 
+
+		$this->load->model('Model_estudiante');
+
+	    $datos_vista = array('rs_estudiantes' => $this->Model_estudiante->VerTodosLosEstudiantes(),'mensaje_confirmacion'=>2);
+
+		$datos_plantilla["cuerpo_central"] = $this->load->view('cuerpo_alumnos_borrar', $datos_vista, true); //Esta es la linea que cambia por cada controlador
+		$datos_plantilla["barra_lateral"] = $this->load->view('templates/barras_laterales/barra_lateral_alumnos', $datos_plantilla, true); //Esta linea tambi?n cambia seg?n la vista como la anterior
+		$this->load->view('templates/template_general', $datos_plantilla);	
+	}
+	
+	/**
+	* Elimina un estudiante del sistema y luego carga los datos para volver a la vista 'cuerpo_alumnos_borrar'
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se llama a la función EliminarEstudiante para eliminar el estudiante con el rut que se le pasa como parametro
+	* y es el que se ha recibido como parametro de esta funcion desde la vista. El resultado de la operación de eliminar desde el modelo se recibe en la variable 'confirmacion'
+	* que se le envía a la vista a través de la variable 'mensaje_confirmacion' para que de el feedback al usuario, en la vista, de como resulto la operación.
+	* Luego se cargan los datos de la vista con la lista 'rs_estudiantes' que contiene toda la información de los estudiantes.
+	* Finalmente se carga la vista con todos los datos.
+	*
+	* @param string $rut_estudiante
+	*/
 	public function eliminarAlumno($rut_estudiante)// alimina un alumno y de ahí carga la vista para seguir eliminando 
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -79,7 +147,16 @@ class Alumnos extends CI_Controller {
 		$this->load->view('templates/template_general', $datos_plantilla);
 	}
 
-
+	/**
+	* Manda a la vista 'cuerpo_alumnos_agregar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se cargan los datos de la vista con la lista 'carreras' y 'secciones' que contienen los datos necesarios para que el usuario
+	* escoja en la vista estos parametros necesarios para ingresar un nuevo alumno. También se envía un mensaje de confirmación con valor 2, que indica que se está cargando
+	* por primera ves la vista de cargar alumnos. Finalmente se carga la vista con todos los datos.
+	*
+	*/
 	public function agregarAlumnos()//carga la vista agregar alumnos
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -114,6 +191,20 @@ class Alumnos extends CI_Controller {
 
 	}
 
+	
+	/**
+	* Inserta un estudiante al sistema y luego carga los datos para volver a la vista 'cuerpo_alumnos_agregar'
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se llama a la función InsertarEstudiante para ingresar al estudiante
+	* con los datos que se capturan un paso antes en el controlador desde la vista con el uso del POST.
+	* El resultado de esta se recibe en la variable 'confirmacion'
+	* que se le envía a la vista a través de la variable 'mensaje_confirmacion' para que de el feedback al usuario, en la vista, de como resulto la operación.
+	* Luego se cargan los datos de la vista con la lista 'carreras' y 'secciones' para que esté habilitada para nuevos ingresos.
+	* Finalmente se carga la vista con todos los datos.
+	*
+	*/
 	public function insertarAlumno()//inserta alumno
 	{
 
@@ -157,37 +248,19 @@ class Alumnos extends CI_Controller {
 		$this->load->view('templates/template_general', $datos_plantilla);
 	}
 
-	public function borrarAlumnos()//carga la vista para borrar alumnos
-	{
-		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
-		if ($rut == FALSE) {
-			redirect('/Login/', ''); //Se redirecciona a login si no tiene sesi?n iniciada
-		}
-		$datos_plantilla["rut_usuario"] = $this->session->userdata('rut');
-		$datos_plantilla["nombre_usuario"] = $this->session->userdata('nombre_usuario');
-		$datos_plantilla["tipo_usuario"] = $this->session->userdata('tipo_usuario');
-		$datos_plantilla["title"] = "ManteKA";
-		$datos_plantilla["menuSuperiorAbierto"] = "Alumnos";
-		$datos_plantilla["head"] = $this->load->view('templates/head', $datos_plantilla, true);
-		$datos_plantilla["barra_usuario"] = $this->load->view('templates/barra_usuario', $datos_plantilla, true);
-		$datos_plantilla["banner_portada"] = $this->load->view('templates/banner_portada', '', true);
-		$datos_plantilla["menu_superior"] = $this->load->view('templates/menu_superior', $datos_plantilla, true);
-		$datos_plantilla["barra_navegacion"] = $this->load->view('templates/barra_navegacion', '', true);
-		$datos_plantilla["mostrarBarraProgreso"] = FALSE; //Cambiar en caso que no se necesite la barra de progreso
-		$datos_plantilla["barra_progreso_atras_siguiente"] = $this->load->view('templates/barra_progreso_atras_siguiente', $datos_plantilla, true);
-		$datos_plantilla["footer"] = $this->load->view('templates/footer', '', true);
 
-
-		$this->load->model('Model_estudiante');
-
-	    $datos_vista = array('rs_estudiantes' => $this->Model_estudiante->VerTodosLosEstudiantes(),'mensaje_confirmacion'=>2);
-
-		$datos_plantilla["cuerpo_central"] = $this->load->view('cuerpo_alumnos_borrar', $datos_vista, true); //Esta es la linea que cambia por cada controlador
-		$datos_plantilla["barra_lateral"] = $this->load->view('templates/barras_laterales/barra_lateral_alumnos', $datos_plantilla, true); //Esta linea tambi?n cambia seg?n la vista como la anterior
-		$this->load->view('templates/template_general', $datos_plantilla);	
-	}
-
-	public function editarAlumnos()//carga ka vista para editar alumnos
+	/**
+	* Manda a la vista 'cuerpo_alumnos_editar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se cargan los datos de la vista con la lista 'secciones' que contienen los datos necesarios para que el usuario
+	* escoja en la vista este parametro para  editar la información del alumno. También se envía un mensaje de confirmación con valor 2, que indica que se está cargando
+	* por primera ves la vista de editar alumnos. Se envía también la lista de todos los estudiantes para que de ahí se escoja al que se quiere editar en la vista.
+	* Finalmente se carga la vista con todos los datos.
+	*
+	*/
+	public function editarAlumnos()//carga la vista para editar alumnos
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
 		if ($rut == FALSE) {
@@ -219,6 +292,20 @@ class Alumnos extends CI_Controller {
 
 	}
 
+	
+	/**
+	* Edita la información de un estudiante del sistema y luego carga los datos para volver a la vista 'cuerpo_alumnos_editar'
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Siguiente a esto se cargan los datos para las plantillas de la página.
+	* Se carga el modelo de estudiantes, se llama a la función ActualizarEstudiante para editar el estudiante
+	* con los datos que se capturan un paso antes en el controlador desde la vista con el uso del POST.
+	* El resultado de esta operacion se recibe en la variable 'confirmacion'
+	* que se le envía a la vista a través de la variable 'mensaje_confirmacion' para que de el feedback al usuario, en la vista, de como resulto la operación.
+	* Luego se cargan los datos de la vista con la lista 'secciones' y 'rs_estudiantes' para que esté habilitada para nuevas ediciones.
+	* Finalmente se carga la vista con todos los datos.
+	*
+	*/
 	public function EditarEstudiante()//edita estudiante
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -263,7 +350,7 @@ class Alumnos extends CI_Controller {
 		$this->load->view('templates/template_general', $datos_plantilla);	
 	}
 	
-			public function cambiarSeccionAlumnos()
+	public function cambiarSeccionAlumnos()
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
 		if ($rut == FALSE) {
@@ -299,8 +386,6 @@ class Alumnos extends CI_Controller {
 	}
 
 	
-	
-
 	public function index() //Esto hace que el index sea la vista que se desee
 	{
 		$this->verAlumnos();
