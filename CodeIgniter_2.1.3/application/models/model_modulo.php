6022f6b0f4bd8c9f0e1867bd0584f84fd23453f9
cristian cornejo <cristian.cornejo@usach.cl>
2013-05-22 01:16:13 -0400

    agregar modulo listo
    está listo agregar modulo, aunque no se puede probar, ya que faltan
    sesiones en la DB.

--- CodeIgniter_2.1.3/application/models/model_modulo.php
+++ CodeIgniter_2.1.3/application/models/model_modulo.php
@@ -16,11 +16,11 @@ class Model_modulo extends CI_Model {
 	* @return array $lista Contiene la información de todos los módulos del sistema
 	*/
 
-	public function VerModulos(){
+	public function VerModulos(){//esta función será modificada
 		$sql="SELECT * FROM MODULO_TEMATICO"; 
 		$datos=mysql_query($sql); 
 		$contador = 0;
-		$lista;
+		$lista = array();
 		while ($row=mysql_fetch_array($datos)) { //Bucle para ver todos los registros
 			$lista[$contador][0] = $row['COD_MODULO_TEM'];
 			$lista[$contador][1] = $row['RUT_USUARIO2'];
@@ -32,6 +32,89 @@ class Model_modulo extends CI_Model {
 		}
 		return $lista;
 	}
+	public function listaNombreModulos(){
+   		$query = $this->db->get('modulo_tematico');
+   		$ObjetoListaResultados = $query->result();
+   		$StringResultados = array();
+   		$contador = 0;
+   		foreach ($ObjetoListaResultados as $row) {
+   			$StringResultados[$contador] = $row->NOMBRE_MODULO;
+            $contador++;
+   		}
+   		return $StringResultados;  	
+	}
+	public function listaSesionesParaAddModulo(){
+		$sql="SELECT * FROM MODULO_TEMATICO"; 
+		$datos=mysql_query($sql); 
+		$contador = 0;
+		$lista = array();
+		while ($row=mysql_fetch_array($datos)) { //Bucle para ver todos los registros
+			$lista[$contador] = array();
+			$lista[$contador][0] = $row['COD_SESION'];
+			$lista[$contador][1] = $row['COD_MODULO_TEM'];
+			$lista[$contador][2] = $row['DESCRIPCION'];
+			$contador = $contador + 1;
+		}
+		return $lista;
+	}
+	public function InsertarModulo($nombre_modulo,$sesiones,$descripcion_modulo,$profesor_lider,$equipo_profesores){
+			//0 insertar modulo
+			$data = array(					
+					'RUT_USUARIO2' => $profesor_lider ,
+					'NOMBRE_MODULO' => $nombre_modulo ,
+					'DESCRIPCION_MODULO' => $descripcion_modulo 
+					);
+			$confirmacion0 = $this->db->insert('modulo_tematico',$data);
+			//
+			$cod_modulo = $this->db->insert_id();
+			
+			//1 insertar equipo
+			$data = array(					
+					'COD_MODULO_TEM' => $cod_modulo
+				);
+			$confirmacion1 = $this->db->insert('equipo_profesor',$data);
+			//
+			$cod_equipo = $this->db->insert_id();	
+			
+			//2 insertar equipo profesores			
+			$contador = 0;
+			$confirmacion2 = true;
+			while ($contador<count($equipo_profesores)){
+			$data = array(					
+					'RUT_USUARIO2' => $equipo_profesores[$contador],
+					'COD_EQUIPO' => $cod_equipo,
+					'LIDER_PROFESOR' => $profesor_lider 
+					);
+			$datos = $this->db->insert('profe_equi_lider',$data);
+				if($datos != true){
+					$confirmacion2 = false;
+				}
+	
+			$contador = $contador + 1;
+			}
+			//3 asignar modulo a sesiones
+			$contador = 0;
+			$confirmacion3 = true;
+			while ($contador<count($sesiones)){
+			$data = array(					
+					'COD_MODULO_TEM' => $cod_modulo
+					);
+					$this->db->where('COD_SESION', $sesiones[$contador]);
+					$datos = $this->db->update('sesion',$data);
+
+				if($datos != true){
+					$confirmacion3 = false;
+				}
+	
+				$contador = $contador + 1;
+			}
+			if($confirmacion0 == false || $confirmacion1 == false || $confirmacion2 == false || $confirmacion3 == false){
+				return -1;
+				}
+			return 1;
+	}
+	
+	
 
 	public function ModificarModulo($cod_modulo_tem,$rut_usuario2,$cod_equipo,$nombre_modulo,$descripcion_modulo)
 	{
