3bddda22cc80ccb14327ea52e5d32c311f6bb720
cristian cornejo <cristian.cornejo@usach.cl>
2013-05-26 19:09:20 -0400

    cambio en eliminar Alumno
    uso de post en vez de get
    esto hace que no se vea el rut por la url

--- CodeIgniter_2.1.3/application/views/cuerpo_alumnos_borrar.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_alumnos_borrar.php
@@ -40,8 +40,8 @@
 					}
 					else{
 		
-					var borrar = document.getElementById("formBorrar");
-					borrar.action = "<?php echo site_url("Alumnos/EliminarAlumno/") ?>/"+rut;
+					var borrar = document.getElementById("FormBorrar");
+					borrar.action ="<?php echo site_url("Alumnos/eliminarAlumno/");?>"
 					borrar.submit();
 					}
 		}
@@ -93,6 +93,7 @@ function ordenarFiltro(){
 
 <div class= "row-fluid">
 	<div class= "span10">
+		<form id="FormBorrar" type="post" onsubmit="eliminarAlumno()"  >
 		<fieldset>
 			<legend>Borrar Alumno</legend>
 			<div class= "row-fluid">
@@ -113,7 +114,7 @@ function ordenarFiltro(){
 
 							<div class="span6">
 									
-									<select id="tipoDeFiltro" title="Tipo de filtro" name="Filtro a usar">
+									<select id="tipoDeFiltro" title="Tipo de filtro" >
 									<option value="1">Filtrar por Nombre</option>
 									<option value="3">Filtrar por Apellido paterno</option>
 									<option value="4">Filtrar por Apellido materno</option>
@@ -167,7 +168,6 @@ function ordenarFiltro(){
 					<div style="margin-bottom:0%">
 							2.-Detalle del Alumno:
 					</div>
-					<form id="formBorrar" type="post">
 					<div class="row-fluid">
 						<div >
 						<pre style="margin-top: 2%; padding: 2%">
@@ -178,32 +178,33 @@ Apellido materno: <b id="apellidomaternoDetalle"></b>
 Carrera:          <b id="carreraDetalle" ></b>
 Sección:          <b id="seccionDetalle"></b>
 Correo:           <b id="correoDetalle"></b></pre>
-<input type="hidden" id="rutEliminar" value="">
+<input name="rut_estudiante" type="hidden" id="rutEliminar" value="">
 								
 						</div>
 					</div>
 					<div class="row" style="margin-top: 2%">
 	
 							<div class="span3 offset6">
-								<button class="btn" onclick="eliminarAlumno()" style="width: 93px" >
+								<button class="btn" type="submit" style="width: 93px">
 									<div class= "btn_with_icon_solo">b</div>
 									&nbsp Borrar
 								</button>
 							</div>
 
 							<div class = "span3 ">
-								<button  class ="btn" type="reset"  style="width: 105px" onclick="DetalleAlumno('','','','','','','','')" >
+								<button  class ="btn" type="reset" style="width: 105px">
 									<div class= "btn_with_icon_solo">Â</div>
 									&nbsp Cancelar
 								</button>
 							</div>
 
 					</div>
-					</form>
+					
 				</div>	
 			</div>
 
 			
 		</fieldset>
+		</form>
 	</div>
 </div>
