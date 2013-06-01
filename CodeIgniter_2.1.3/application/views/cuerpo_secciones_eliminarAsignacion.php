a49a3d0ebcd520e331bd772c30f0838d8d002dac
Marco Acevedo <marco.acevedo@usach.cl>
2013-06-01 02:08:13 -0400

    vista eliminar asignacion

---
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_eliminarAsignacion.php
@@ -0,0 +1,71 @@
+<div class="row-fluid">
+	<div class="span10">
+		<fieldset>
+			<legend>Borrar Asignación</legend>
+			<div class="row-fluid">
+				<div class="span6">
+					<div class="row-fluid">
+						<div class="span9">
+							<div class="control-group">
+								<label class="control-label" for="inputInfo">1-.<font color="red">*</font> Seleccione la sección </label>
+							</div>
+						</div>
+					</div>
+
+					<div class="row-fluid">
+						<div class="span10" style="border:#cccccc 1px solid; overflow-y:scroll; height:200px; -webkit-border-radius: 4px;">
+							<table id="listadoResultados" class="table table-hover">
+								<thead>
+									<tr>
+										
+									</tr>
+								</thead>
+								<tbody>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="span6">
+					<div class="row-fluid">
+						<div class="span9">
+							<div class="control-group">
+								<label class="control-label" for="inputInfo">2-. Datos de la asignación </label>
+							</div>
+						</div>
+					</div>
+					<div class="row-fluid">
+
+
+						<pre style=" padding: 2%">
+Sección:          
+Módulo Actual:              
+Profesor Lider:          
+Profesor Asignado: 
+Sala Asignada: 
+Horario Asignado:        
+						</pre>
+					</div>
+
+					<div class="row-fluid">
+						<div class="span3 offset6">
+							<button class="btn" type="submit" style="width: 93px">
+								<div class= "btn_with_icon_solo">b</div>
+								&nbsp Borrar
+							</button>
+						</div>
+
+						<div class = "span3 ">
+							<button  class ="btn" type="reset"  style="width: 105px">
+								<div class= "btn_with_icon_solo">Â</div>
+								&nbsp Cancelar
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</fieldset>
+	</div>
+</div>
