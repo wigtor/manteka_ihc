c49b5ef8123967a84435aa2e470b1c4dee9e978c
Daniel Quinteros <daniel.quinteros@usach.cl>
2013-06-04 23:01:02 -0400

    Planificación funcionando
    :D

--- CodeIgniter_2.1.3/application/models/model_planificacion.php
+++ CodeIgniter_2.1.3/application/models/model_planificacion.php
@@ -36,9 +36,9 @@ class model_planificacion extends CI_Model {
 		}
 		return $filas; */
 
-		$columnas = 'seccion.COD_SECCION, profesor.NOMBRE1_PROFESOR, profesor.NOMBRE2_PROFESOR, profesor.APELLIDO1_PROFESOR, profesor.APELLIDO2_PROFESOR, modulo_tematico.NOMBRE_MODULO, sesion.DESCRIPCION_SESION, sala.NUM_SALA, horario.COD_DIA, horario.COD_MODULO';
-		$condiciones = '(sala_horario.COD_SECCION = seccion.COD_SECCION) AND (sala_horario.COD_SALA = sala.COD_SALA) AND (sala_horario.COD_HORARIO = horario.COD_HORARIO) AND (sesion.COD_SECCION = seccion.COD_SECCION) AND (sesion.COD_MODULO_TEM = modulo_tematico.COD_MODULO_TEM) AND (modulo_tematico.COD_EQUIPO = equipo_profesor.COD_EQUIPO) AND (equipo_profesor.COD_EQUIPO = profe_equi_lider.COD_EQUIPO) AND (profe_equi_lider.LIDER_PROFESOR = 1) AND (profe_equi_lider.RUT_USUARIO2 = profesor.RUT_USUARIO2) AND (modulo_tematico.COD_MODULO_TEM = equipo_profesor.COD_MODULO_TEM)';
-		$desde = '`seccion`, `profesor`, `modulo_tematico`, `sesion`, `sala`, `horario`, `sala_horario`, `equipo_profesor`, `profe_equi_lider`';
+		$columnas = 'seccion.COD_SECCION, profesor.NOMBRE1_PROFESOR, profesor.NOMBRE2_PROFESOR, profesor.APELLIDO1_PROFESOR, profesor.APELLIDO2_PROFESOR, modulo_tematico.NOMBRE_MODULO, sala.NUM_SALA, horario.NOMBRE_HORARIO, dia.NOMBRE_DIA, modulo.NUMERO_MODULO';
+		$condiciones = '(seccion_mod_tem.COD_SECCION = seccion.COD_SECCION) AND (seccion_mod_tem.COD_MODULO_TEM  = modulo_tematico.COD_MODULO_TEM) AND (seccion_mod_tem.ID_HORARIO_SALA = sala_horario.ID_HORARIO_SALA) AND (sala_horario.COD_SALA = sala.COD_SALA) AND (sala_horario.COD_HORARIO = horario.COD_HORARIO) AND (modulo_tematico.COD_EQUIPO = equipo_profesor.COD_EQUIPO) AND (equipo_profesor.COD_EQUIPO = profe_equi_lider.COD_EQUIPO) AND (profe_equi_lider.LIDER_PROFESOR = 1) AND (profe_equi_lider.RUT_USUARIO2 = profesor.RUT_USUARIO2) AND (modulo_tematico.COD_MODULO_TEM = equipo_profesor.COD_MODULO_TEM) AND (dia.COD_DIA = horario.COD_DIA) AND (modulo.COD_MODULO = horario.COD_MODULO)';
+		$desde = '`seccion`, `profesor`, `modulo_tematico`, `sala`, `horario`, `sala_horario`, `equipo_profesor`, `profe_equi_lider`, `seccion_mod_tem`, `modulo`, `dia`';
 
 		$query = $this->db->select($columnas);
 		
@@ -69,8 +69,8 @@ class model_planificacion extends CI_Model {
 		return $lista;
 	}
 
-	SELECT seccion.COD_SECCION, profesor.NOMBRE1_PROFESOR, profesor.NOMBRE2_PROFESOR, profesor.APELLIDO1_PROFESOR, profesor.APELLIDO2_PROFESOR, modulo_tematico.NOMBRE_MODULO, sesion.DESCRIPCION_SESION, sala.NUM_SALA, horario.COD_DIA, horario.COD_MODULO FROM `seccion`, `profesor`, `modulo_tematico`, `sesion`, `sala`, `horario`, `sala_horario`, `equipo_profesor`, `profe_equi_lider` WHERE (sala_horario.COD_SECCION = seccion.COD_SECCION) AND (sala_horario.COD_SALA = sala.COD_SALA) AND (sala_horario.COD_HORARIO = horario.COD_HORARIO) AND (sesion.COD_SECCION = seccion.COD_SECCION) AND (sesion.COD_MODULO_TEM = modulo_tematico.COD_MODULO_TEM) AND (modulo_tematico.COD_EQUIPO = equipo_profesor.COD_EQUIPO) AND (equipo_profesor.COD_EQUIPO = profe_equi_lider.COD_EQUIPO) AND (profe_equi_lider.LIDER_PROFESOR = 1) AND (profe_equi_lider.RUT_USUARIO2 = profesor.RUT_USUARIO2) AND (modulo_tematico.COD_MODULO_TEM = equipo_profesor.COD_MODULO_TEM);	*/
-
+SELECT seccion.COD_SECCION, profesor.NOMBRE1_PROFESOR, profesor.NOMBRE2_PROFESOR, profesor.APELLIDO1_PROFESOR, profesor.APELLIDO2_PROFESOR, modulo_tematico.NOMBRE_MODULO, sala.NUM_SALA, horario.NOMBRE_HORARIO, dia.NOMBRE_DIA, modulo.NUMERO_MODULO FROM `seccion`, `profesor`, `modulo_tematico`, `sala`, `horario`, `sala_horario`, `equipo_profesor`, `profe_equi_lider`, `seccion_mod_tem`, `modulo`, `dia` WHERE (seccion_mod_tem.COD_SECCION = seccion.COD_SECCION) AND (seccion_mod_tem.COD_MODULO_TEM  = modulo_tematico.COD_MODULO_TEM) AND (seccion_mod_tem.ID_HORARIO_SALA = sala_horario.ID_HORARIO_SALA) AND (sala_horario.COD_SALA = sala.COD_SALA) AND (sala_horario.COD_HORARIO = horario.COD_HORARIO) AND (modulo_tematico.COD_EQUIPO = equipo_profesor.COD_EQUIPO) AND (equipo_profesor.COD_EQUIPO = profe_equi_lider.COD_EQUIPO) AND (profe_equi_lider.LIDER_PROFESOR = 1) AND (profe_equi_lider.RUT_USUARIO2 = profesor.RUT_USUARIO2) AND (modulo_tematico.COD_MODULO_TEM = equipo_profesor.COD_MODULO_TEM) AND (dia.COD_DIA = horario.COD_DIA) AND (modulo.COD_MODULO = horario.COD_MODULO);
+*/
 
 }
 
