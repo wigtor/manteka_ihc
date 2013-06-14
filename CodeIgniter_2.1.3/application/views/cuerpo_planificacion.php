e5fa39fa8f3e5f12de4a504e119d218d2501eb2d
Marco Acevedo <marco.acevedo@usach.cl>
2013-06-14 01:31:21 -0400

    Arreglo Planificación
    Arreglé el modelo de planificación que estaba malo

--- CodeIgniter_2.1.3/application/views/cuerpo_planificacion.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_planificacion.php
@@ -17,17 +17,20 @@
 			<?php
 
 			$columnas = 'seccion.COD_SECCION, profesor.NOMBRE1_PROFESOR, profesor.NOMBRE2_PROFESOR, profesor.APELLIDO1_PROFESOR, profesor.APELLIDO2_PROFESOR, modulo_tematico.NOMBRE_MODULO, sesion.DESCRIPCION_SESION, sala.NUM_SALA, horario.COD_DIA, horario.COD_MODULO';
-			foreach ($lista as $fila) {
+			//foreach ($lista as $fila) {
+			$contador=0;
+			while($contador<count($lista)){
 				echo '<tr>';
-				echo '<td>'.$fila['COD_SECCION'].'</td>';
-				echo '<td>'.$fila['NOMBRE1_PROFESOR'].' '.$fila['NOMBRE2_PROFESOR'].' '.$fila['APELLIDO1_PROFESOR'].' '.$fila['APELLIDO2_PROFESOR'].'</td>';
-				echo '<td>'.$fila['NOMBRE_MODULO'].'</td>';
-				echo '<td>'.$fila['NUM_SALA'].'</td>';
-				echo '<td>'.$fila['NOMBRE_HORARIO'].'</td>';
-				echo '<td>'.$fila['NUMERO_MODULO'].'</td>';
-				echo '<td>'.$fila['NOMBRE_DIA'].'</td>';
+				echo '<td>'.$lista[$contador][0].'</td>';
+				echo '<td>'.$lista[$contador][1].' '.$lista[$contador][2].' '.$lista[$contador][3].'</td>';
+				echo '<td>'.$lista[$contador][4].'</td>';
+				echo '<td>'.$lista[$contador][5].'</td>';
+				echo '<td>'.$lista[$contador][6].'</td>';
+				echo '<td>'.$lista[$contador][7].'</td>';
+				echo '<td>'.$lista[$contador][8].'</td>';
 
 				echo '</tr>';
+				$contador=$contador+1;
 			}
 
 								
