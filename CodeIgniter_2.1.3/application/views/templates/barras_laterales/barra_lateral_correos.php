f8de7d83dd943fde73a4a7e69bfdc0399509c4f5
Victor Flores <victor.floress@usach.cl>
2013-06-18 00:58:51 -0400

    Agregado link a log de eliminados

--- CodeIgniter_2.1.3/application/views/templates/barras_laterales/barra_lateral_correos.php
+++ CodeIgniter_2.1.3/application/views/templates/barras_laterales/barra_lateral_correos.php
@@ -22,6 +22,7 @@
 		$agregarGrupos = "";
 		$editarGrupos = "";
 		$borrarGrupos = "";
+		$logEliminados = "";
 
 		// Variables que determinan que grupo de operaciones se encuentra abierta
 		$inCorreos = "";
@@ -75,50 +76,55 @@
 			$borrarGrupos = 'class="active"';
 			$inGrupos = "in";
 		}
+		else if ($subVistaLateralAbierta == "logEliminados") {
+			$logEliminados = 'class="active"';
+			$inCorreos = "in";
+		}
 	?>
 
 	<!--	Barra lateral de correos	-->
-	<div class="accordion" id="accordion2">
-    	<div class="accordion-group">
-		    <div class="accordion-heading">
-				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-				Correos</a>
-		    </div>
-		    <div id="collapseOne" class="accordion-body collapse <?php echo $inCorreos; ?>">
-		    	<div class="accordion-inner nav nav-list">
-		    		<li <?php echo $correosRecibidos; ?> ><a href="<?php echo site_url("Correo/correosRecibidos")?>">Correos recibidos</a></li>
-		        	<li <?php echo $correosEnviados; ?> ><a href="<?php echo site_url("Correo/correosEnviados")?>">Correos enviados</a></li>
-	        		<li <?php echo $enviarCorreo; ?> ><a href="<?php echo site_url("Correo/enviarCorreo")?>">Enviar correo</a></li>
-					<li <?php echo $verBorradores; ?> ><a href="<?php echo site_url("Correo/verBorradores")?>">Ver borradores</a></li>
-		     	</div>
-		    </div>
-	  	</div>
-		<div class="accordion-group">
-		    <div class="accordion-heading">
-				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-				Plantillas</a>
-		    </div>
-		    <div id="collapseTwo" class="accordion-body collapse <?php echo $inPlantillas; ?>">
-		    	<div class="accordion-inner nav nav-list">
-		        	<li <?php echo $agregarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/agregarPlantillas")?>">Agregar plantillas</a></li>
-					<li <?php echo $editarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/editarPlantillas")?>">Editar plantillas</a></li>
-					<li <?php echo $borrarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/borrarPlantillas")?>">Borrar plantillas</a></li>
-		     	</div>
-		    </div>
-	  </div>
-	  <div class="accordion-group">
-		    <div class="accordion-heading">
-				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
-				Grupos de contactos</a>
-		    </div>
-		    <div id="collapseThree" class="accordion-body collapse <?php echo $inGrupos; ?>">
-		    	<div class="accordion-inner nav nav-list">
-		        	<li <?php echo $verGrupos; ?> ><a href="<?php echo site_url("GruposContactos/verGrupos")?>">Ver grupos</a></li>
-					<li <?php echo $agregarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/agregarGrupos")?>">Agregar grupos</a></li>
-					<li <?php echo $editarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/editarGrupos")?>">Editar grupos</a></li>
-					<li <?php echo $borrarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/borrarGrupos")?>">Borrar grupos</a></li>
-		     	</div>
-		    </div>
-	  </div>
-	</div>
+		<div class="accordion" id="accordion2">
+			<div class="accordion-group">
+				<div class="accordion-heading">
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+						Correos</a>
+				</div>
+				<div id="collapseOne" class="accordion-body collapse <?php echo $inCorreos; ?>">
+					<div class="accordion-inner nav nav-list">
+						<li <?php echo $correosRecibidos; ?> ><a href="<?php echo site_url("Correo/correosRecibidos")?>">Correos recibidos</a></li>
+						<li <?php echo $correosEnviados; ?> ><a href="<?php echo site_url("Correo/correosEnviados")?>">Correos enviados</a></li>
+						<li <?php echo $enviarCorreo; ?> ><a href="<?php echo site_url("Correo/enviarCorreo")?>">Enviar correo</a></li>
+						<li <?php echo $verBorradores; ?> ><a href="<?php echo site_url("Correo/verBorradores")?>">Ver borradores</a></li>
+						<li <?php echo $logEliminados; ?> ><a href="<?php echo site_url("Correo/logEliminados")?>">Log de eliminados</a></li>
+					</div>
+				</div>
+			</div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+					Plantillas</a>
+				</div>
+				<div id="collapseTwo" class="accordion-body collapse <?php echo $inPlantillas; ?>">
+					<div class="accordion-inner nav nav-list">
+						<li <?php echo $agregarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/agregarPlantillas")?>">Agregar plantillas</a></li>
+						<li <?php echo $editarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/editarPlantillas")?>">Editar plantillas</a></li>
+						<li <?php echo $borrarPlantillas; ?> ><a href="<?php echo site_url("Plantillas/borrarPlantillas")?>">Borrar plantillas</a></li>
+					</div>
+				</div>
+			</div>
+			<div class="accordion-group">
+				<div class="accordion-heading">
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+					Grupos de contactos</a>
+				</div>
+				<div id="collapseThree" class="accordion-body collapse <?php echo $inGrupos; ?>">
+					<div class="accordion-inner nav nav-list">
+						<li <?php echo $verGrupos; ?> ><a href="<?php echo site_url("GruposContactos/verGrupos")?>">Ver grupos</a></li>
+						<li <?php echo $agregarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/agregarGrupos")?>">Agregar grupos</a></li>
+						<li <?php echo $editarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/editarGrupos")?>">Editar grupos</a></li>
+						<li <?php echo $borrarGrupos; ?> ><a href="<?php echo site_url("GruposContactos/borrarGrupos")?>">Borrar grupos</a></li>
+					</div>
+				</div>
+			</div>
+		</div>
 
