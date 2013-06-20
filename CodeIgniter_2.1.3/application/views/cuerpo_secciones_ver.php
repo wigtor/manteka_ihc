a1501af87ee3323cd5a9b89f416c77dde190a366
Marco Acevedo <marco.acevedo@usach.cl>
2013-06-19 23:36:44 -0400

    Eliminacion de detalles 2 XD sorry

--- CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_secciones_ver.php
@@ -243,22 +243,9 @@ Bloque:  <b id="modulo"></b>
                                         <th class="span9">Nombres</th>
                                     </tr>
                                 </thead>
-                                    <!-- esta fila es solo de ejemplo-->
+                                    
                                 <tbody>
-                                    	<?php
-										$contador=0;
-										while ($contador<count($rs_estudiantes)){
-											echo '<tr>';
-											echo '<td id="rs_estudiantesTd_'.$contador.'" > '.$rs_estudiantes[$contador][7].' </td>';
-											echo '<td id="rs_estudiantesTd_'.$contador.'" > '.$rs_estudiantes[$contador][0].' </td>';
-											echo '<td id="rs_estudiantesTd_'.$contador.'" > '.$rs_estudiantes[$contador][3].' </td>';
-											echo '<td id="rs_estudiantesTd_'.$contador.'" > '.$rs_estudiantes[$contador][4].' </td>';
-											echo '<td id="rs_estudiantesTd_'.$contador.'" > '.$rs_estudiantes[$contador][1].' '.$rs_estudiantes[$contador][2].' </td>';
-											echo '</tr>';
-											$contador = $contador + 1;
-										}
-										?>
-									
+                                    	
                                 </tbody>
                                 
                             </table>
