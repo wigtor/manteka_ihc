fd7d6ae6e245b2ff370f1759e02f96fc39f839f2
Aldo Navarrete <aldo.navarrete@usach.cl>
2013-06-21 01:25:35 -0400

    Logo Usach
    Abrir en nueva ventana

--- CodeIgniter_2.1.3/application/views/templates/banner_portada.php
+++ CodeIgniter_2.1.3/application/views/templates/banner_portada.php
@@ -1,7 +1,7 @@
 <!-- Contiene la imágen de logo de la usach a la izquierda y la imagen de logo de manteka a la derecha, todo en un fondo gradiente -->
 <div class="banner row-fluid">
 		<div class="pull-left">
-			<a href="http://www.udesantiago.cl"><img src="/<?php echo config_item('dir_alias') ?>/img/logo_usach.png" class="logo_banner" alt="logo usach"></a>
+			<a href="http://www.udesantiago.cl" target="blank"><img src="/<?php echo config_item('dir_alias') ?>/img/logo_usach.png" class="logo_banner" alt="logo usach"></a>
 		</div>
 		<div class="pull-right">
 			<img src="/<?php echo config_item('dir_alias') ?>/img/logo_manteka.png" class="logo_banner" alt="logo manteka">
