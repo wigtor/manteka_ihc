4249255e97920bdce015cbfafa0f9515027c9db6
Aldo Navarrete <aldo.navarrete@usach.cl>
2013-06-25 19:56:41 -0400

    Avance About Manteka
    Grupo 1 y Grupo 4 añadidos

--- CodeIgniter_2.1.3/application/views/cuerpo_about.php
+++ CodeIgniter_2.1.3/application/views/cuerpo_about.php
@@ -1,3 +1,39 @@
-<fieldset>
+<fieldset align="center">
 		<legend>Acerca de Nosotros</legend>
+    <div id="textAbout" align="left">
+      <p>Nosotros somos el equipo desarrollador de MANTEKA, somos alumnos del profesor Edmundo Leiva Lobos de la asignatura
+        Interacción Humano-Computador impartida el primer semestre del año 2013 en la <a href="http://www.udesantiago.cl" target="blank">Universidad de Santiago de Chile</a>. 
+        </p>
+      <p>El proyecto MANTEKA fue desarrollado para la asignatura de Comunicación Efectiva. El objetivo del sistema es gestionar la comunicación y planificación de las secciones que suman un total de más de mil alumnos. 
+       El trabajo fue dividido en cuatro grupos encargados de distintas áreas:</p>
+    </div>
+		<div id="myCarousel" class="carousel slide" align="center">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <div class="active item">
+    	<img src="/<?php echo config_item('dir_alias') ?>/img/about_img/phppDo97CPM.jpg">
+      <div class="carousel-caption" align="left">
+        <h4>Grupo 1</h4>
+        <p>....</p>
+      </div> 
+    </div>
+    <div class="item">
+    	<img src="/<?php echo config_item('dir_alias') ?>/img/about_img/php1oWgkVPM.jpg">
+      <div class="carousel-caption" align="left">
+        <h4>Grupo 4</h4>
+        <p>....</p>
+      </div> 
+    </div>
+   
+  <!-- Carousel nav -->
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div>
 </fieldset>
+
+
