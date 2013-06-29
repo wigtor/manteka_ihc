8b591af4a314118f6152fbba133cc98514c788cd
Diego Escobar Lagos <diego.escobarla@usach.cl>
2013-06-29 05:38:34 -0400

    documentacion completa de controlador sesiones

--- CodeIgniter_2.1.3/application/controllers/Sesiones.php
+++ CodeIgniter_2.1.3/application/controllers/Sesiones.php
@@ -26,20 +26,39 @@ class Sesiones extends MasterManteka {
 		$this->verSesiones();
 	}
 
-
+ 	 
+	/**
+	* Manda a la vista 'cuerpo_sesiones_ver' los datos necesarios para su funcionamiento
+	*
+	* Primero se carga los datos de la vista, en este caso con 0 poeque no se recibe ningun dato del modelo.
+	* Luego, se determina el menu lateral correspondiente a verSesiones, para que quede seleccionado en el menu.
+	* A continuacion, se indica que la vista no tendra la barra de progreso.
+	* Posteriormente, se comprueba que el usuario tenga la sesión iniciada y que sea coordinador, en caso que no sea así se le redirecciona al login
+	* Finalmente se carga la vista con todos los datos como parámetros en la funcion cargarTodo.
+	*/
 	public function verSesiones()
 	{
+		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
+		if ($rut == FALSE) {
+			redirect('/Login/', ''); //Se redirecciona a login si no tiene sesi?n iniciada
+		}
 		$datos_vista = 0;		
 		$subMenuLateralAbierto = "verSesiones"; 
 		$muestraBarraProgreso = FALSE; //Indica si se muestra la barra que dice anterior - siguiente
 		$tipos_usuarios_permitidos = array();
 		$tipos_usuarios_permitidos[0] = TIPO_USR_COORDINADOR; $tipos_usuarios_permitidos[1] = TIPO_USR_PROFESOR;
-
-
-		$this->load->model('Model_sesiones');
-
 		$this->cargarTodo("Planificacion", 'cuerpo_sesiones_ver', "barra_lateral_planificacion", $datos_vista, $tipos_usuarios_permitidos, $subMenuLateralAbierto, $muestraBarraProgreso);
 	}
+	
+	/**
+	* Manda a la vista 'cuerpo_sesiones_agregar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Seguido a esto, se carga los datos de la vista, en este caso con 0 poeque no se recibe ningun dato del modelo.
+	* Luego, se determina el menu lateral correspondiente a agregarSesiones, para que quede seleccionado en el menu.
+	* A continuacion, se indica que la vista no tendra la barra de progreso.
+	* Finalmente se carga la vista con todos los datos como parámetros en la funcion cargarTodo.
+	*/
 	public function ingresarSesiones()
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -56,6 +75,18 @@ class Sesiones extends MasterManteka {
 
 	}
 
+	/**
+	* Inserta una sesion al sistema y luego carga los datos para volver a la vista 'cuerpo_sesiones_agregar'
+	*
+	* Se carga el modelo de sesiones, y a continuación se obtienen los datos de nombre y descripción de la sesion. 
+	* Con los dichos datos que se capturan un paso antes en el controlador desde la vista con el uso del POST,
+	* Se llama a la funcion AgregarSesion en el modelo.
+	* El resultado de ésta, se recibe en la variable 'confirmacion'
+	* A partir del valor de 'confirmacion' se ejecuta eñ mensaje accion realizada o accion no realizada.
+	* Finalmente, se llama a la vista a la cual se redirecciona desde el mensaje de retroalimentacion, se especifica el submenu que estara seleccinado,
+	* se determina el tipo de usuario y se llama a la funcion cargarMsjLogueado con los datos anteriores.
+	*
+	*/
 	public function agregarSesiones()
 	{
 		$this->load->model('Model_sesiones');
@@ -84,6 +115,14 @@ class Sesiones extends MasterManteka {
 		$this->cargarMsjLogueado($datos_plantilla, $tipos_usuarios_permitidos);
 	}
 
+    /**
+	* Comprobar que el nombre de la sesion que se ingresando no pertenezca a otra sesion existente ya en el sistema
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Luego, se carga el model de sesiones y se captura el nombre de la sesion
+	* Finalmente se llama a la función nombreExisteM que reside en el modelo, que retorna -1 
+	* Si el nombre de la sesión existe y 1 en caso contrario.
+	* @return json Resultado de la busqueda en forma de objeto json
+	*/
     public function nombreExisteC() {
 		if (!$this->isLogged()) {
 			//echo 'No estás logueado!!';
@@ -97,6 +136,13 @@ class Sesiones extends MasterManteka {
 	}
 
 	
+    /**
+	* Obtener el detalle de una sesion determinada
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le 
+	* Redirecciona al login. Siguiente a esto, se carga el model de sesion, se captura el codigo de la
+	* Sesion. Finalmente se llama a la función getDetallesSesion del modelo, que retorna el detalle de la sesion.
+	* @return json Resultado de la busqueda en forma de objeto json
+	*/
     public function postDetallesSesion() {
 		//Se comprueba que quien hace esta petición de ajax esté logueado
 		if (!$this->isLogged()) {
@@ -110,6 +156,14 @@ class Sesiones extends MasterManteka {
 		echo json_encode($resultado);
 	}
 
+	/**
+	* Obtener los datos de las sesiones que coincidan con la búsqueda del filtro seleccionado.
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le 
+	* Redirecciona al login. Siguiente a esto, se capturan los tipos de filtros seleccionados. luego
+	* Se carga el model de sesion, se realiza una nueva búsqueda dado el filtro básico, y una por cada filtro avanzado seleccionado. 
+	* Finalmente se retorna el resultado de la búsqueda, dados los filtros seleccionados.
+	* @return json Resultado de la busqueda en forma de objeto json
+	*/
 	public function postBusquedaSesiones() {
 		if (!$this->isLogged()) {
 			//echo 'No estás logueado!!';
@@ -125,6 +179,19 @@ class Sesiones extends MasterManteka {
 		echo json_encode($resultado);
 	}
     
+    /**
+	* Elimina una sesion del sistema y luego carga los datos para volver a la vista 'cuerpo_sesiones_eliminar'
+	*
+	* Se carga el modelo de sesiones, y a continuación se obtienen los datos de codigo de la sesion. 
+	* Dicho dato se capturan un paso antes en el controlador desde la vista con el uso del POST,
+	* Se llama a la funcion EliminarSesion en el modelo.
+	* El resultado de ésta, se recibe en la variable 'confirmacion'
+	* A partir del valor de 'confirmacion' se ejecuta eñ mensaje accion realizada o accion no realizada.
+	* Finalmente, se llama a la vista a la cual se redirecciona, en este caso borrarSesiones, desde el mensaje de retroalimentacion;
+	* Se especifica el submenu que estara seleccinado,
+	* Se determina el tipo de usuario y se llama a la funcion cargarMsjLogueado con los datos anteriores.
+	*
+	*/
     public function eliminarSesion()// alimina un alumno y de ahí carga la vista para seguir eliminando 
 	{
 
@@ -153,17 +220,42 @@ class Sesiones extends MasterManteka {
 		$this->cargarMsjLogueado($datos_plantilla, $tipos_usuarios_permitidos);
 	}
     
+    /**
+	* Manda a la vista 'cuerpo_sesiones_eliminar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Seguido a esto, se carga los datos de la vista, en este caso con 0 poeque no se recibe ningun dato del modelo.
+	* Luego, se determina el menu lateral correspondiente a borrarSesiones, para que quede seleccionado en el menu.
+	* A continuacion, se indica que la vista no tendra la barra de progreso.
+	* Finalmente se carga la vista con todos los datos como parámetros en la funcion cargarTodo.
+	*/
     public function borrarSesiones()//carga la vista para borrar alumnos
 	{
-		$this->load->model('Model_sesiones');
-		$datos_vista = array('mensaje_confirmacion'=>2);//$datos_vista = array('mensaje_confirmacion'=>2);
-
+		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
+		if ($rut == FALSE) {
+			redirect('/Login/', ''); //Se redirecciona a login si no tiene sesi?n iniciada
+		}
+		$datos_vista = 0;
 		$subMenuLateralAbierto = "borrarSesiones"; //Para este ejemplo, los informes no tienen submenu lateral
 		$muestraBarraProgreso = FALSE; //Indica si se muestra la barra que dice anterior - siguiente
 		$tipos_usuarios_permitidos = array();
 		$tipos_usuarios_permitidos[0] = TIPO_USR_COORDINADOR;
 		$this->cargarTodo("Planificacion", 'cuerpo_sesiones_eliminar', "barra_lateral_planificacion", $datos_vista, $tipos_usuarios_permitidos, $subMenuLateralAbierto, $muestraBarraProgreso);	
 	}
+	
+	/**
+	* Edita una sesion del sistema y luego carga los datos para volver a la vista 'cuerpo_sesiones_editar'
+	*
+	* Se carga el modelo de sesiones, y a continuación se obtienen los datos de codigo, nombre y descripcion de la sesión. 
+	* Dichos datos se capturan un paso antes en el controlador desde la vista con el uso del POST,
+	* Se llama a la funcion EditarSesion en el modelo.
+	* El resultado de ésta, se recibe en la variable 'confirmacion'
+	* A partir del valor de 'confirmacion' se ejecuta eñ mensaje accion realizada o accion no realizada.
+	* Finalmente, se llama a la vista a la cual se redirecciona, en este caso editarSesiones, desde el mensaje de retroalimentacion;
+	* Se especifica el submenu que estara seleccinado,
+	* Se determina el tipo de usuario y se llama a la funcion cargarMsjLogueado con los datos anteriores.
+	*
+	*/
 	public function cambiarSesiones()//carga la vista para borrar alumnos
 	{
 		$this->load->model('Model_sesiones');
@@ -191,6 +283,15 @@ class Sesiones extends MasterManteka {
 		$this->cargarMsjLogueado($datos_plantilla, $tipos_usuarios_permitidos);
 	}
 
+	/**
+	* Manda a la vista 'cuerpo_sesiones_editar' los datos necesarios para su funcionamiento
+	*
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Seguido a esto, se carga los datos de la vista, en este caso con 0 poeque no se recibe ningun dato del modelo.
+	* Luego, se determina el menu lateral correspondiente a editarSesiones, para que quede seleccionado en el menu.
+	* A continuacion, se indica que la vista no tendra la barra de progreso.
+	* Finalmente se carga la vista con todos los datos como parámetros en la funcion cargarTodo.
+	*/
 	public function editarSesiones()
 	{
 		$rut = $this->session->userdata('rut'); //Se comprueba si el usuario tiene sesi?n iniciada
@@ -207,6 +308,14 @@ class Sesiones extends MasterManteka {
 
 	}
 
+	/**
+	* Comprobar que el nombre de la sesion que se está editando no pertenezca a otra sesion ingresada en el sistema
+	* Primero se comprueba que el usuario tenga la sesión iniciada, en caso que no sea así se le redirecciona al login
+	* Luego, se carga el model de sesiones, se captura el nombre de la sesion y el código.
+	* Finalmente se llama a la función nombreExisteEM que reside en el modelo, que retorna 1 
+	* Si el nombre de la sesión existe y 0 en caso contrario.
+	* @return json Resultado de la busqueda en forma de objeto json
+	*/
 	public function nombreExisteEC() {
 		if (!$this->isLogged()) {
 			//echo 'No estás logueado!!';
