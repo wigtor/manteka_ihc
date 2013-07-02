eca903b4c012de099216842af50c8b36e3c1f376
Mario <marchim12@hotmail.com>
2013-07-02 03:08:14 -0300

    correcciones undo-redo
    funcionando para todos los inputs de manteka sin importar dentro de que
    se encuentren, ademas gatilla el evento onchange que tenga asociado el
    input si es que se modifica su valor.

--- CodeIgniter_2.1.3/javascripts/undo-redo.js
+++ CodeIgniter_2.1.3/javascripts/undo-redo.js
@@ -9,69 +9,70 @@ var current_state = 0;
 
 var text_elements   = 'input[type="text"],input[type="email"], input[type="password"], textarea';
 var state_elements  = 'input[type="radio"], input[type="checkbox"]';
-var select_elements = 'input[type="number"],input[type="color"],input[type="date"], select ' ;
+var select_elements = 'input[type="number"],input[type="color"],input[type="date"], select, input[type="hidden"]' ;
 
+function saveState(){
+    num_states = current_state;
+    num_states++;
+    $('.undo').removeAttr( "disabled");
+    $('.redo').attr( "disabled", "disabled" );
+    current_state = num_states;
+   
+    $(select_elements+","+text_elements).each(function() {
+        var current_val = $(this).val();
+        var prev = $(this).attr('data-previous').split(',').slice(0,num_states);
+        prev.push(current_val);
+        $(this).attr('data-previous', prev.join(','));
+    });
+    $(state_elements).each(function() {
+        var current_val = $(this).prop('checked');
+        var prev = $(this).attr('data-previous').split(',').slice(0,num_states);
+        prev.push(current_val);
+        $(this).attr('data-previous', prev.join(','));
+    });
+}
  
 $(document).ready(function() {
    
     // Se guardan los valores iniciales
-    $(text_elements, '.undoable form').each(function() {
+    $(text_elements+","+select_elements).each(function() {
         var val = $(this).val();
         $(this).attr('data-previous', val);
     });
-    $(select_elements, '.undoable form').each(function() {
-        var val = $(this).val();
-        $(this).attr('data-previous', val);
-    });
-    $(state_elements, '.undoable form').each(function() {
+    $(state_elements).each(function() {
         var val = $(this).prop('checked');
         $(this).attr('data-previous', val);
     });
    
     // On change se agrega un estado a cada elemento
-    $(state_elements+","+select_elements, '.undoable form').on('change', function() {
-        num_states = current_state;
-        num_states++;
-        $('.undo').removeAttr( "disabled");
-        current_state = num_states;
-       
-        $(select_elements, '.undoable form').each(function() {
-            var current_val = $(this).val();
-            var prev = $(this).attr('data-previous').split(',').slice(0,num_states);
-            prev.push(current_val);
-            $(this).attr('data-previous', prev.join(','));
-        });
-        $(state_elements, '.undoable form').each(function() {
-            var current_val = $(this).prop('checked');
-            var prev = $(this).attr('data-previous').split(',').slice(0,num_states);
-            prev.push(current_val);
-            $(this).attr('data-previous', prev.join(','));
-        });
+   $(state_elements+","+select_elements).on('change', function() {
+        saveState();
     });
-    $(text_elements, '.undoable form').keyup(function() {
-        num_states = current_state;
-        num_states++;
-        $('.undo').removeAttr( "disabled");
-        current_state = num_states;
-       
-        $(text_elements, '.undoable form').each(function() {
-            var current_val = $(this).val();
-            var prev = $(this).attr('data-previous').split(',').slice(0,num_states);
-            prev.push(current_val);
-            $(this).attr('data-previous', prev.join(','));
-        });
+    $(text_elements).keyup(function() {
+        saveState();
     });
    
-    $('.undo, .redo', '.undoable .undoredo').on('click', function() {
+    $('.undo, .redo').on('click', function() {
+        editado = false;
         if ($(this).hasClass('undo')) {
             // undo
             if (current_state == 0)  return false;
             
-            current_state--;
-            $('.redo').removeAttr( "disabled");
-            if(current_state==0){
-                $('.undo').attr( "disabled", "disabled" );
+            if(editado){
+                saveState();
+                current_state--;
+                $('.redo').removeAttr( "disabled");
+                if(current_state==0){
+                    $('.undo').attr( "disabled", "disabled" );
+                }
+            }else{
+                current_state--;
+                $('.redo').removeAttr( "disabled");
+                if(current_state==0){
+                    $('.undo').attr( "disabled", "disabled" );
+                }
             }
+            
         } else {
             // redo
             if (current_state == num_states) return false;
@@ -82,17 +83,22 @@ $(document).ready(function() {
             }
         }
        
-        $(text_elements, '.undoable form').each(function() {
-            var prev = $(this).attr('data-previous').split(',');
+        $(select_elements+","+text_elements).each(function() {
+            
+            var prev   = $(this).attr('data-previous').split(',');
+            var actual = $(this).val();
             $(this).val(prev[current_state]);
+            if(actual != $(this).val()){
+                this.onchange();
+            }
         });
-        $(select_elements, '.undoable form').each(function() {
+        $(state_elements).each(function() {
             var prev = $(this).attr('data-previous').split(',');
-            $(this).val(prev[current_state]);
-        });
-        $(state_elements, '.undoable form').each(function() {
-            var prev = $(this).attr('data-previous').split(',');
+            var actual = $(this).prop('checked');
             $(this).prop('checked', bool(prev[current_state]));
+            if( actual != $(this).prop('checked') ){
+                this.onchange();
+            }
         });
        
         return true;
